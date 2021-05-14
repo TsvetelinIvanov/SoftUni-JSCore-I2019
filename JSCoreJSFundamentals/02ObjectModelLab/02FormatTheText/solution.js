5275aec7845799303e07e366bc27aaecaa5fb1f8
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-05-14 06:45:29 +0300

    Add 02ObjectModelLab

---
+++ JSCoreJSFundamentals/02ObjectModelLab/02FormatTheText/solution.js
@@ -0,0 +1,20 @@
+function solve(){
+  let input = document.getElementById('input').textContent;  
+  let separatedInput = input.split('.');
+  console.log(separatedInput.length)
+  let output = document.getElementById('output');
+  let p = document.createElement('p');
+  for(let i = 0; i < separatedInput.length; i++){
+    if (separatedInput[i].length > 0){
+    p.textContent += separatedInput[i] + '.';
+    }
+    
+    if ((i + 1) % 3 === 0){
+      output.appendChild(p);
+      p = document.createElement('p');
+    }
+    else if (separatedInput.length - i <= 2){
+      output.appendChild(p);
+    }
+  }
+}
---
+++ JSCoreJSFundamentals/02ObjectModelLab/04RegisterNotification/solution.js
@@ -0,0 +1,42 @@
+
+ function register() {
+   let usernameElement = document.getElementById('username');
+   let emailElement = document.getElementById('email');
+   let passwordElement = document.getElementById('password');   
+
+   let usernameInput = usernameElement.value;   
+   let emailInput = emailElement.value;
+   let passwordInput = passwordElement.value;
+  
+   let pattrn = /(.+)@(.+).(com|bg)/gm;
+   if (usernameInput.length > 0 && passwordInput.length > 0 && emailInput.match(pattrn)){
+    let resultElement = document.getElementById('result');
+    
+     let h1Element = document.createElement('h1');
+     h1Element.textContent = 'Successful Registration!'
+     resultElement.appendChild(h1Element);
+    //  let pElementUsername = document.createElement('p');
+    //  pElementUsername.textContent = `Username: ${usernameInput}`;
+    //  resultElement.appendChild(pElementUsername);
+    //  let pElementEmail = document.createElement('p');
+    //  pElementEmail.textContent = `Email: ${emailInput}`
+    //  resultElement.appendChild(pElementEmail);
+    //  let pElementPassword = document.createElement('p');
+    //  pElementPassword.textContent = `Password: ${('*').repeat(passwordInput.length)}`
+    //  resultElement.appendChild(pElementPassword);
+
+    resultElement.innerHTML += `Username: ${usernameInput}`;
+    resultElement.appendChild(document.createElement('br'));
+    resultElement.innerHTML += `Email: ${emailInput}`;
+    resultElement.appendChild(document.createElement('br'));
+    resultElement.innerHTML += `Password: ${('*').repeat(passwordInput.length)}`;
+
+     usernameElement.value = '';
+     emailElement.value = '';
+     passwordElement.value = '';
+
+    setTimeout(() => {
+      resultElement.innerHTML = '';
+    }, 5000);
+   }
+ }
