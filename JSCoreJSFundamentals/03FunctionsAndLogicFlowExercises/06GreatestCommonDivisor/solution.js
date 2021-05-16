87f48012a491171a99c111cae4869f27ac1ff07c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-05-16 12:00:54 +0300

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/06GreatestCommonDivisor/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/06GreatestCommonDivisor/solution.js
@@ -1,20 +1,3 @@
-// function greatestCD() { //75/100 in Judge
-//     let firstNumber = document.getElementById('num1').value;
-//     let secondNumber = document.getElementById('num2').value;
-//     let result = findGreatestCommonDivisor(firstNumber, secondNumber);
-//     let resultElement = document.getElementById('result');
-//     resultElement.innerHTML = result;
-
-//     function findGreatestCommonDivisor(firstNumber, secondNumber){
-//         let lowerNumber = Math.min(firstNumber, secondNumber);
-//         for(let i = lowerNumber; i > 0; i--){
-//             if (firstNumber % i === 0 && secondNumber % i === 0){
-//                 return i;
-//             }
-//         }
-//     }    
-// }
-
 function greatestCD() {
     let num1 = Number(document.getElementById('num1').value);
     let num2 = Number(document.getElementById('num2').value);
