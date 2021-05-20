8e0367d858723c8d4ccd11d745e75a2b8c952888
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-05-20 14:31:22 +0300

    Add ExamJSAdvancedWithResources17III2019

---
+++ JSCoreAdvanced/ExamJSAdvancedWithResources17III2019/01SoftUni Courses/scripts/solution.js
@@ -0,0 +1,90 @@
+function solve() {
+   let $inputJSFundamentals = $('input[name="js-fundamentals"]');
+   let $inputJSAdvanced = $('input[name="js-advanced"]');
+   let $inputJSApplications = $('input[name="js-applications"]');
+   let $inputJSWeb = $('input[name="js-web"]');
+   let $inputRadioOnline = $('input[value="online"]');
+   let $buttonSignMeUp = $('button');
+
+   $buttonSignMeUp.on('click', addCourses);
+
+   function addCourses(){            
+      let $ulMyCourses = $('#myCourses div[class="courseBody"] ul');
+      let $pCost = $('#myCourses div[class="courseFoot"] p'); 
+      
+      let isCheckedInputJSFundamentals = $inputJSFundamentals.is(':checked');
+      let isCheckedInputJSAdvanced = $inputJSAdvanced.is(':checked');
+      let isCheckedInputJSApplications = $inputJSApplications.is(':checked');
+      let isCheckedInputJSWeb = $inputJSWeb.is(':checked');
+      let isCheckedinputRadioOnline = $inputRadioOnline.is(':checked');      
+
+      // let isCheckedInputJSFundamentals;
+      // let isCheckedInputJSAdvanced;
+      // let isCheckedInputJSApplications;
+      // let isCheckedInputJSWeb;
+
+      // $inputJSFundamentals.input(function(){
+      //    if ($(this).is(':checked'))
+      //    isCheckedInputJSFundamentals = true;
+      //  });
+
+      //  $inputJSAdvanced.change(function(){
+      //    if ($(this).is(':checked'))
+      //    isCheckedInputJSAdvanced = true;
+      //  });      
+
+      let cost = 0;
+      if (isCheckedInputJSFundamentals){
+         let $li = $('<li>');
+         $li.text('JS-Fundamentals');
+         $ulMyCourses.append($li);
+         cost += 170;
+      }
+
+      if (isCheckedInputJSAdvanced){
+         let $li = $('<li>');
+         $li.text('JS-Advanced');
+         $ulMyCourses.append($li);
+         cost += 180;
+      }
+
+      if (isCheckedInputJSApplications){
+         let $li = $('<li>');
+         $li.text('JS-Applications');
+         $ulMyCourses.append($li);
+         cost += 190;
+      }
+
+      if (isCheckedInputJSWeb){
+         let $li = $('<li>');
+         $li.text('JS-Web');
+         $ulMyCourses.append($li);
+         cost += 490;
+      }
+
+      if(isCheckedInputJSFundamentals && isCheckedInputJSAdvanced){
+         cost -= 180 * 0.1;
+      }
+
+      if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced && isCheckedInputJSApplications){
+         cost -= 31.32;
+      }
+      
+      if(isCheckedinputRadioOnline){
+         cost -= cost * 0.06;
+      }
+
+      if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced && isCheckedInputJSApplications && isCheckedInputJSWeb){
+         let $li = $('<li>');
+         $li.text('HTML and CSS');
+         $ulMyCourses.append($li);
+      }
+
+      cost = Math.floor(cost);
+
+      $pCost.text(`Cost: ${cost}.00 BGN`);
+   }
+
+}
+//in Judge must be paste without this below:
+solve();
---
+++ JSCoreAdvanced/ExamJSAdvancedWithResources17III2019/02FilmStudio/test/02FilmStudio-tests.js
@@ -0,0 +1,133 @@
+let expect = require('chai').expect;
+let FilmStudio = require('../02FilmStudio').FilmStudio;
+//In Judge must be paste without this above
+describe('SoftUniFy', function () {
+    describe('constructor', function () {
+        it('should have property name', function () {
+            let filmStudio = new FilmStudio('Verginia');
+            expect(filmStudio).to.haveOwnProperty('name');
+        });
+        it('should have correct property name', function () {
+            let filmStudio = new FilmStudio('Verginia');
+            expect(filmStudio.name).to.be.equal('Verginia');
+        });
+        it('should have property films', function () {
+            let filmStudio = new FilmStudio();
+            expect(filmStudio).to.haveOwnProperty('films');
+        });
+        it('should have property films empty array', function () {
+            let filmStudio = new FilmStudio();
+            expect(JSON.stringify(filmStudio.films)).to.be.equal('[]');
+        });
+        it('should have property films empty array', function () {
+            let filmStudio = new FilmStudio();
+            expect(filmStudio.films).to.be.eql([]);
+        });
+        it('should have property films empty array', function () {
+            let filmStudio = new FilmStudio('Verginia');
+            expect(JSON.stringify(filmStudio.films)).to.be.equal('[]');
+        });
+        it('should have property films empty array', function () {
+            let filmStudio = new FilmStudio('Verginia');
+            expect(filmStudio.films).to.be.eql([]);
+        });
+    });
+
+    describe('makeMovie(filmName, roles)', function () {
+        let filmStudio;
+        beforeEach(function () {
+            filmStudio = new FilmStudio('Verginia');
+        });
+
+        it('should throw by invalid arguments count', function () {
+            expect(() => { filmStudio.makeMovie('Vanilla scay'); }).to.throw('Invalid arguments count');
+        });
+        it('should throw by invalid arguments count', function () {
+            expect(() => { filmStudio.makeMovie('Vanilla scay', ['roles'], 'wrongArgument'); }).to.throw('Invalid arguments count');
+        });
+        it('should throw by invalid argument filmName', function () {
+            expect(() => { filmStudio.makeMovie(9, ['roles']); }).to.throw('Invalid arguments');
+            expect(() => { filmStudio.makeMovie([], ['roles']); }).to.throw('Invalid arguments');
+            expect(() => { filmStudio.makeMovie(true, ['roles']); }).to.throw('Invalid arguments');
+        });
+        it('should throw by invalid argument roles', function () {
+            expect(() => { filmStudio.makeMovie('Vanilla scay', 'roles'); }).to.throw('Invalid arguments');
+            expect(() => { filmStudio.makeMovie('Vanilla scay', { 'roles': 1 }); }).to.throw('Invalid arguments');
+            expect(() => { filmStudio.makeMovie('Vanilla scay', 1); }).to.throw('Invalid arguments');
+        });
+
+        it('should return corect value for one film ', function () {
+            expect(filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy'])).to.be.eql({
+                filmName: 'The Avengers',
+                filmRoles:
+                    [{ role: 'Iron-Man', actor: false },
+                    { role: 'Thor', actor: false },
+                    { role: 'Hulk', actor: false },
+                    { role: 'Arrow guy', actor: false }]
+            }
+            );
+        });
+        it('should return corect value for one existing film ', function () {
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy']);
+            expect(filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Hulk', 'Arrow guy', 'Ant-man'])).to.be.eql({
+                filmName: 'The Avengers 2',
+                filmRoles:
+                    [{ role: 'Iron-Man', actor: false },
+                    { role: 'Hulk', actor: false },
+                    { role: 'Arrow guy', actor: false },
+                    { role: 'Ant-man', actor: false }]
+            }
+            );
+        });
+        it('should return corect value for one new film by existing films', function () {
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy']);
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Hulk', 'Arrow guy', 'Ant-man']);
+            expect(filmStudio.makeMovie('The New Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy', 'Black Panther'])).to.be.eql({
+                filmName: 'The New Avengers',
+                filmRoles:
+                    [{ role: 'Iron-Man', actor: false },
+                    { role: 'Thor', actor: false },
+                    { role: 'Hulk', actor: false },
+                    { role: 'Arrow guy', actor: false },
+                    { role: 'Black Panther', actor: false }]
+            }
+            );
+        });
+    });
+
+    describe('casting(actor, role)', function () {
+        let filmStudio;
+        beforeEach(function () {
+            filmStudio = new FilmStudio('Verginia');
+        });
+
+        it('should return notsuccess message for no films', function () {
+            expect(filmStudio.casting('Leo De Caprio', 'detectiv')).to.be.equal(`There are no films yet in Verginia.`);
+        });
+        it('should return notsuccess message for no roles', function () {
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy']);
+            expect(filmStudio.casting('Leo De Caprio', 'detectiv')).to.be.equal(`Leo De Caprio, we cannot find a detectiv role...`);
+        });
+        it('should return success message for find role', function () {
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy']);
+            expect(filmStudio.casting('Leo De Caprio', 'Thor')).to.be.equal(`You got the job! Mr. Leo De Caprio you are next Thor in the The Avengers. Congratz!`);
+        });
+    });
+
+    describe('lookForProducer(film)', function () {
+        let filmStudio;
+        beforeEach(function () {
+            filmStudio = new FilmStudio('Verginia');
+        });
+
+        it('should throw error for not finded film', function () {
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy']);
+            expect(() => { filmStudio.lookForProducer('The Vilage'); }).to.throw(`The Vilage do not exist yet, but we need the money...`);
+        });
+        
+        it('should return correct finded film', function () {
+            filmStudio.makeMovie('The Avengers', ['Iron-Man', 'Thor', 'Hulk', 'Arrow guy']);
+            expect(filmStudio.lookForProducer('The Avengers')).to.be.equal('Film name: The Avengers\nCast:\nfalse as Iron-Man\nfalse as Thor\nfalse as Hulk\nfalse as Arrow guy\n');
+        });
+    });
+});
