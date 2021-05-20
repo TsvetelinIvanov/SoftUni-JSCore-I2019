d7d6b131a14b08b1bc5bde844a7689095bb0af07
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-05-20 13:40:59 +0300

    Add 05TemplatingExerciseWhitResources

---
+++ JSCoreApplications/05TemplatingExerciseWhitResources/01ListTowns/towns.js
@@ -0,0 +1,20 @@
+function attachEvents(){
+    $('#btnLoadTowns').click(async () => {
+        let $towns = $('#towns');
+        let towns = $towns.val().split(',').filter(t => t !== '').map(t => {
+            let object = {town: t};
+            return object;
+        });
+
+        if (towns){
+            let source = $('#towns-template').html();
+            let template = Handlebars.compile(source);
+            let context = {towns};
+
+            let html = template(context);
+
+            $('#root').html(html);
+            $towns.val('');
+        }
+    });
+}
