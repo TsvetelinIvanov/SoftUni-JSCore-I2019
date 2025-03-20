69a27f2daf4f16c8bec51fdd8c38cbf1f3a013b8
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-20 15:35:30 +0200

    Update 03Fruit.js

--- JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/03Fruit.js
+++ JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/03Fruit.js
@@ -2,7 +2,9 @@ function buyFruits(fruit, weightInGramsString, pricePerKilogramString){
     let weightInKilograms = Number(weightInGramsString) / 1000;
     let pricePerKilogram = Number(pricePerKilogramString);
     let price = weightInKilograms * pricePerKilogram;
+    
     console.log(`I need ${price.toFixed(2)} leva to buy ${weightInKilograms.toFixed(2)} kilograms ${fruit}.`);
 }
+
 buyFruits('orange', 2500, 1.80)
 buyFruits('apple', 1563, 2.35)
