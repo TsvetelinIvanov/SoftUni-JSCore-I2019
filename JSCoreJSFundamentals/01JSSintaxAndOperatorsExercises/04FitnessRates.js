550808f538b27762f0f4cf3982d7ce1236a9635b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-20 15:46:31 +0200

    Update 04FitnessRates.js

--- JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/04FitnessRates.js
+++ JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/04FitnessRates.js
@@ -2,27 +2,48 @@ function showServicePrice(weekDay, service, hourString){
     let hour = Number(hourString);    
     if (hour < 8.0 || hour > 22.0){
         console.log('The gym does not work in this time!')
+        
         return;
    }
+    
     let price = 0;
     switch(weekDay){
         case 'Monday':
         case 'Tuesday':
         case 'Wednesday':
         case 'Thursday':
-        case 'Friday': if (hour > 15.0) {
+        case 'Friday': 
+            if (hour > 15.0) {
             price += 2.5;
-        } switch(service){
-            case 'Fitness': price += 5; break;
-            case 'Sauna': price += 4; break;
-            case 'Instructor': price += 10; break;
-        } break;
+        }
+            switch(service){
+            case 'Fitness':
+                    price += 5;
+                    break;
+            case 'Sauna':
+                    price += 4;
+                    break;
+            case 'Instructor':
+                    price += 10;
+                    break;
+        }
+            
+            break;
         case 'Saturday':
-        case 'Sunday': switch(service){
-            case 'Fitness': price = 8; break;
-            case 'Sauna': price = 7; break;
-            case 'Instructor': price = 15; break;
-        } break;
+        case 'Sunday':
+            switch(service){
+            case 'Fitness':
+                    price = 8;
+                    break;
+            case 'Sauna':
+                    price = 7;
+                    break;
+            case 'Instructor':
+                    price = 15;
+                    break;
+        }
+            
+            break;
     }
 
     console.log(price);
