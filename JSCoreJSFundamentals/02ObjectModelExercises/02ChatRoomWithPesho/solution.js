040ac6959844dbfbfa07fb35bcb1c418446e0e1e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-23 16:12:42 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/02ChatRoomWithPesho/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/02ChatRoomWithPesho/solution.js
@@ -1,17 +1,18 @@
 function solve() {
  let buttons = Array.from(document.getElementsByTagName('button'));
  let inputs = Array.from(document.getElementsByTagName('input'));
+ 
  buttons.forEach((button) => {
     button.addEventListener('click', function (event) {
         let div = document.createElement('div');
         let span = document.createElement('span');
         let p = document.createElement('p');
-        let senderButton = event.target.name;
-        if (senderButton === 'myBtn'){
+        let senderButtonName = event.target.name;
+        if (senderButtonName === 'myBtn'){
             span.textContent = 'Me';
             p.textContent = document.getElementById('myChatBox').value;
             div.style.textAlign = 'left';
-        } else if (senderButton === 'peshoBtn'){
+        } else if (senderButtonName === 'peshoBtn'){
             span.textContent = 'Pesho';
             p.textContent = document.getElementById('peshoChatBox').value;
             div.style.textAlign = 'right';
@@ -21,6 +22,7 @@ function solve() {
         div.appendChild(p);
         let chatChronology = document.getElementById('chatChronology');
         chatChronology.appendChild(div);
+     
         inputs[0].value = '';
         inputs[1].value = '';
     })
