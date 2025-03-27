dcc8947a202ba2cc3a43a198787de960b9109d59
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-27 09:10:07 +0200

    Update 02EvenNumbers1ToN.js

--- JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/02EvenNumbers1ToN.js
+++ JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/02EvenNumbers1ToN.js
@@ -1,7 +1,7 @@
-function evenNumbers(n){
+function evenNumbers(n) {
     let number = Number(n);
-    for(let i = 1; i <= number; i++){
-        if (i % 2 === 0){
+    for (let i = 1; i <= number; i++) {
+        if (i % 2 === 0) {
             console.log(i)
         }
     }
