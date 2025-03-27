ba8079af0c9af79bf24c1de9fd23b16def1c9a11
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-27 09:19:17 +0200

    Update 01DailyCalorieIntake.js

--- JSCoreJSFundamentals/01JSyntaxAndOperationsMoreExercises/01DailyCalorieIntake.js
+++ JSCoreJSFundamentals/01JSyntaxAndOperationsMoreExercises/01DailyCalorieIntake.js
@@ -1,32 +1,32 @@
-function calculateDailyCalorieIntake(personData, workoutsCount){
+function calculateDailyCalorieIntake(personData, workoutsCount) {
     let sex = personData[0];
     let weight = Number(personData[1]);
     let height = Number(personData[2]);
     let age = Number(personData[3]);
     
     let caloriesBasicMetabolism = 0;
-    if (sex === 'm'){
+    if (sex === 'm') {
         caloriesBasicMetabolism = 66 + 13.8 * weight + 5 * height - 6.8 * age;
     }
-    else if (sex === 'f'){
+    else if (sex === 'f') {
         caloriesBasicMetabolism = 655 + 9.7 * weight + 1.85 * height - 4.7 * age;
     }
 
     let workoutsCountNumber = Number(workoutsCount);
     let activeFactor = 0;
-    if (workoutsCountNumber < 1){
+    if (workoutsCountNumber < 1) {
         activeFactor = 1.2;
     }
-    else if (workoutsCountNumber <= 2){
+    else if (workoutsCountNumber <= 2) {
         activeFactor = 1.375;
     }
-    else if (workoutsCountNumber <= 5){
+    else if (workoutsCountNumber <= 5) {
         activeFactor = 1.55;
     }
-    else if (workoutsCountNumber <= 7){
+    else if (workoutsCountNumber <= 7) {
         activeFactor = 1.725;
     }
-    else if (workoutsCountNumber > 7){
+    else if (workoutsCountNumber > 7) {
         activeFactor = 1.9;
     }
    
