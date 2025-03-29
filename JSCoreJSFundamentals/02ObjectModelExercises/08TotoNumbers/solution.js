fa4f75023732117a9275ea1bc1d245fd8a6c2117
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-29 17:08:53 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/08TotoNumbers/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/08TotoNumbers/solution.js
@@ -1,33 +1,36 @@
 function solve() {
-	let button = document.querySelector('button');
-	button.addEventListener('click', addTotoNumbers);
-	function addTotoNumbers () {
-		let inputField = document.querySelector('input');
-		let inputArray = Array.from(inputField.value.split(' '));
-		let numbers = inputArray.map(Number);
+    let button = document.querySelector('button');
+    button.addEventListener('click', addTotoNumbers);
+	
+    function addTotoNumbers () {
+	let inputField = document.querySelector('input');
+	let inputArray = Array.from(inputField.value.split(' '));
+	let numbers = inputArray.map(Number);
 		
-		if (numbers.length !== 6){
-			return;
-		}
-
-		for (let number of numbers){
-			if (number < 1 || number > 49){
-				return;
-			}
-		}
-
-		//numbers.sort((a,b) => a - b);
-		let allNumbersDiv = document.getElementById('allNumbers');
-		for (let i = 1; i <= 49; i++) {
-			let numberDiv = document.createElement('div');
-			numberDiv.textContent = i;
-			numberDiv.classList.add('numbers');
-			if (numbers.includes(i)){
-				numberDiv.style.backgroundColor = 'orange';
-			}
-			allNumbersDiv.appendChild(numberDiv);
-		}
-		button.setAttribute('disabled', true);
-		inputField.setAttribute('disabled', true);		
+	if (numbers.length !== 6){
+	    return;
 	}
+
+	for (let number of numbers) {
+	    if (number < 1 || number > 49){
+		return;
+	    }
+	}
+
+	//numbers.sort((a,b) => a - b);
+	let allNumbersDiv = document.getElementById('allNumbers');
+	for (let i = 1; i <= 49; i++) {
+	    let numberDiv = document.createElement('div');
+	    numberDiv.textContent = i;
+	    numberDiv.classList.add('numbers');
+	    if (numbers.includes(i)) {
+		numberDiv.style.backgroundColor = 'orange';
+	    }
+		
+	    allNumbersDiv.appendChild(numberDiv);
+	}
+	    
+	button.setAttribute('disabled', true);
+	inputField.setAttribute('disabled', true);		
+    }
 }
