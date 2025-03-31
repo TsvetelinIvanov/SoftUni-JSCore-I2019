e218ed5ebeab503cb53ce036bf77ccd2786546fe
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-03-31 16:13:14 +0300

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/10NoSignal/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/10NoSignal/solution.js
@@ -1,19 +1,19 @@
 function solve() {
-	let exerciseDiv = document.getElementById('exercise');
-	let noSignalDiv = exerciseDiv.getElementsByTagName('div')[0];
+    let exerciseDiv = document.getElementById('exercise');
+    let noSignalDiv = exerciseDiv.getElementsByTagName('div')[0];
 
-	// setTimeout(() => {//it change its position only once
-	// 	let horizontalRandomNumber = Math.floor(Math.random() * 81) + 1;
-	// 	let verticalRandomNumber = Math.floor(Math.random() * 45) + 1;
-	// 	noSignalDiv.style.position = `absolute`;
-	// 	noSignalDiv.style.top = `${horizontalRandomNumber}%`;
-	// 	noSignalDiv.style.left = `${verticalRandomNumber}vh`;
-	// }, 2000);
-	setInterval(function () {
-		let horizontalRandomNumber = Math.floor(Math.random() * 81) + 1;
-		let verticalRandomNumber = Math.floor(Math.random() * 45) + 1;
-		noSignalDiv.style.position = `absolute`;
-		noSignalDiv.style.top = `${horizontalRandomNumber}%`;
-		noSignalDiv.style.left = `${verticalRandomNumber}vh`;
-	}, 2000);
+    // setTimeout(() => {//it changes its position only once
+    // 	    let horizontalRandomNumber = Math.floor(Math.random() * 81) + 1;
+    // 	    let verticalRandomNumber = Math.floor(Math.random() * 45) + 1;
+    // 	    noSignalDiv.style.position = `absolute`;
+    // 	    noSignalDiv.style.top = `${horizontalRandomNumber}%`;
+    // 	    noSignalDiv.style.left = `${verticalRandomNumber}vh`;
+    // }, 2000);
+    setInterval(function () {
+	let horizontalRandomNumber = Math.floor(Math.random() * 81) + 1;
+	let verticalRandomNumber = Math.floor(Math.random() * 45) + 1;
+	noSignalDiv.style.position = `absolute`;
+	noSignalDiv.style.top = `${horizontalRandomNumber}%`;
+	noSignalDiv.style.left = `${verticalRandomNumber}vh`;
+    }, 2000);
 }
