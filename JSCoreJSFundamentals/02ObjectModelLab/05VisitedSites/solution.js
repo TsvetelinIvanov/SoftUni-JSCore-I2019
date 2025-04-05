44a51172000a585ee8497968de0427a3e02a6c69
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-05 08:31:46 +0300

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelLab/05VisitedSites/solution.js
+++ JSCoreJSFundamentals/02ObjectModelLab/05VisitedSites/solution.js
@@ -1,13 +1,13 @@
 function solve() {
-  let aElementsArray = Array.from(document.getElementsByTagName('a'));
-  aElementsArray.forEach(a => {
-    a.addEventListener('click', () => {
-      let spanElement = a.parentNode.children[1];
-      let spanElementArray = spanElement.textContent.split(' ');
-      let visitedCounter = Number(spanElementArray[1]);
-      visitedCounter++
-      spanElementArray[1] = visitedCounter;
-      spanElement.textContent = spanElementArray.join(' ');
+    let aElementsArray = Array.from(document.getElementsByTagName('a'));
+    aElementsArray.forEach(a => {
+        a.addEventListener('click', () => {
+            let spanElement = a.parentNode.children[1];
+            let spanElementArray = spanElement.textContent.split(' ');
+            let visitedCounter = Number(spanElementArray[1]);
+            visitedCounter++
+            spanElementArray[1] = visitedCounter;
+            spanElement.textContent = spanElementArray.join(' ');
+        });
     });
-  });
 }
