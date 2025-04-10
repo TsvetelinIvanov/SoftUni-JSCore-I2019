80aaeb31b5aa0094e5de2d61ea5e282e73a892e6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-10 11:02:56 +0300

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/06GreatestCommonDivisor/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/06GreatestCommonDivisor/solution.js
@@ -3,6 +3,16 @@ function greatestCD() {
     let num2 = Number(document.getElementById('num2').value);
     let result = document.getElementById('result');
 
+    //function gcd(a, b) {
+    //    while (b !== 0) {
+    //        let temp = b;
+    //        b = a % b;
+    //        a = temp;
+    //    }
+        
+    //    return a;
+    //}
+
     function gcd(a, b) {
         if (!b) {
             return a;
