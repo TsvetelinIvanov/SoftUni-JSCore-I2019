830ab79e2b80ae3d478765fc7cf100537cf7781a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-11 09:29:13 +0300

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/07BinarySearch/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/07BinarySearch/solution.js
@@ -3,56 +3,57 @@
 //     let inputIntegerArray = JSON.parse(inputIntegerArrayElement.value);
 //     let numberToSearchElement = document.getElementById('num');
 //     let numberToSearch = Number(numberToSearchElement.value);
-//     let resultElement = document.getElementById('result');
-    
-//     if (inputIntegerArray.indexOf(numberToSearch) === -1){
+//     let resultElement = document.getElementById('result');    
+//     if (inputIntegerArray.indexOf(numberToSearch) === -1) {
 //         resultElement.innerHTML = numberToSearch + ' is not in the array';
-//     } else {
+//     } 
+//     else {
 //         resultElement.innerHTML = `Found ${numberToSearch} at index ${inputIntegerArray.indexOf(numberToSearch)}`;
-//     }  
-    
+//     }    
 // }
 
 function binarySearch() {
     let inputIntegerArrayElement = document.getElementById('arr');
-    let inputrString = inputIntegerArrayElement.value;
-    //inputrString = inputrString.substring(1, inputrString.length - 1);
-    inputIntegerArray = inputrString.split(', ').map(Number);
+    let inputString = inputIntegerArrayElement.value;
+    //inputString = inputrString.substring(1, inputString.length - 1);
+    inputIntegerArray = inputString.split(', ').map(Number);
     console.log(inputIntegerArray);
+    
     let numberToSearchElement = document.getElementById('num');
     let numberToSearch = Number(numberToSearchElement.value);
-    let resultElement = document.getElementById('result');
     
-    if (inputIntegerArray.indexOf(numberToSearch) === -1){
+    let resultElement = document.getElementById('result');    
+    if (inputIntegerArray.indexOf(numberToSearch) === -1) {
         resultElement.innerHTML = numberToSearch + ' is not in the array';
-    } else {
+    } 
+    else {
         resultElement.innerHTML = `Found ${numberToSearch} at index ${inputIntegerArray.indexOf(numberToSearch)}`;
-    }  
-    
+    }    
 }
 
 // function binarySearch() {
 //     let inputIntegerArrayElement = document.getElementById('arr');
 //     let inputrString = inputIntegerArrayElement.value;
 //     //inputrString = inputrString.substring(1, inputrString.length - 1);
-//     inputIntegerArray = inputrString.split(', ');    
+//     inputIntegerArray = inputrString.split(', ');
+
 //     let numberToSearchElement = document.getElementById('num');
 //     let numberToSearch = Number(numberToSearchElement.value);
-//     let resultElement = document.getElementById('result');
-    
+
+//     let resultElement = document.getElementById('result');    
 //     let isInArray = false;
 //     let index;
 //     for (let i = 0; i < inputIntegerArray.length; i++) {
-//         if(+inputIntegerArray[i] === numberToSearch){
+//         if (+inputIntegerArray[i] === numberToSearch) {
 //             isInArray = true;
 //             index = i;
 //         }
 //     }
-//     if (isInArray === true){
-//         resultElement.innerHTML = `Found ${numberToSearch} at index ${index}`;
-//     } else {
-//         resultElement.innerHTML = numberToSearch + ' is not in the array';
-//     }  
-    
-// }
 
+//     if (isInArray === true) {
+//         resultElement.innerHTML = `Found ${numberToSearch} at index ${index}`;
+//     }
+//     else {
+//         resultElement.innerHTML = numberToSearch + ' is not in the array';
+//     }    
+// }
