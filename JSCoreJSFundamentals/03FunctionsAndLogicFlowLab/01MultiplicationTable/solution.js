b8b490a65c987764105ca9a9ba998a71358a025b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-15 12:39:03 +0300

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowLab/01MultiplicationTable/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowLab/01MultiplicationTable/solution.js
@@ -1,23 +1,23 @@
 function solve() {
-  let multipliedStartNumberElement = document.getElementById('num1');
-  let multiplicationStartNumber = Number(multipliedStartNumberElement.value);
-  let multiplierElement = document.getElementById('num2');
-  let multiplier = Number(multiplierElement.value);
-  let resultElement = document.getElementById('result');
+    let multipliedStartNumberElement = document.getElementById('num1');
+    let multiplicationStartNumber = Number(multipliedStartNumberElement.value);
+    let multiplierElement = document.getElementById('num2');
+    let multiplier = Number(multiplierElement.value);
+    let resultElement = document.getElementById('result');
   
-  resultElement.innerHTML = '';
-  findWrongInput(multiplicationStartNumber, multiplier);
-  showResults(multiplicationStartNumber, multiplier)
+    resultElement.innerHTML = '';
+    findWrongInput(multiplicationStartNumber, multiplier);
+    showResults(multiplicationStartNumber, multiplier)
 
-  function findWrongInput(multiplicationStartNumber, multiplier){
-    if (multiplicationStartNumber > multiplier){
-      //document.getElementById('result').innerHTML = 'Try with other numbers.';
-      resultElement.innerHTML = 'Try with other numbers.';
+    function findWrongInput(multiplicationStartNumber, multiplier) {
+        if (multiplicationStartNumber > multiplier) {
+        //document.getElementById('result').innerHTML = 'Try with other numbers.';
+        resultElement.innerHTML = 'Try with other numbers.';
     }
   }
 
-  function showResults(multiplicationStartNumber, multiplier){
-    for (let i = multiplicationStartNumber; i <= multiplier; i++) {
+  function showResults(multiplicationStartNumber, multiplier) {
+      for (let i = multiplicationStartNumber; i <= multiplier; i++) {
       let result = i * multiplier;
       let p = document.createElement('p');
       p.innerHTML = `${i} * ${multiplier} = ${result}`;
