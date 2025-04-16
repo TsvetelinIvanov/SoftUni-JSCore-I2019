1a19966dc5b3a638c2c3cfafe85c6ec40956237a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-16 12:18:08 +0300

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowLab/03CountOccurrencesOfAGivenCharacter/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowLab/03CountOccurrencesOfAGivenCharacter/solution.js
@@ -1,26 +1,27 @@
 function solve() {
-  let string = document.getElementById('string').value;
-  let character = document.getElementById('character').value;
-  let count = 0;
-  let result = '';
+    let string = document.getElementById('string').value;
+    let character = document.getElementById('character').value;
+    let count = 0;
+    let result = '';
 
-  findCharacterCount(string, character);
-  showEvenOrOdd();
-  document.getElementById('result').innerHTML = result;
+    findCharacterCount(string, character);
+    showEvenOrOdd();
+    document.getElementById('result').innerHTML = result;
 
-  function findCharacterCount(string, character){
-    for (let i = 0; i < string.length; i++) {
-      if (string[i] === character){
-        count++;
-      }      
+    function findCharacterCount(string, character) {
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === character) {
+                count++;
+            }      
+        }
     }
-  }
 
-  function showEvenOrOdd(){
-    if (count % 2 === 0){
-      result = `Count of ${character} is even.`;
-    } else {
-      result = `Count of ${character} is odd.`;
+    function showEvenOrOdd() {
+        if (count % 2 === 0) {
+            result = `Count of ${character} is even.`;
+        }
+        else {
+            result = `Count of ${character} is odd.`;
+        }
     }
-  }
 }
