440952576c6b4dbb80a0b170c3ef8d6a9512b7ab
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-23 06:16:06 +0300

    Update 02PrintEveryNthElementFromArray.js

--- JSCoreJSFundamentals/04ArraysAndMatricesExercises/02PrintEveryNthElementFromArray.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesExercises/02PrintEveryNthElementFromArray.js
@@ -1,4 +1,4 @@
-function printEveryNthElement (arr){
+function printEveryNthElement(arr) {
     let step = Number(arr.pop());
     for (let i = 0; i < arr.length; i += step) {
         console.log(arr[i]);        
