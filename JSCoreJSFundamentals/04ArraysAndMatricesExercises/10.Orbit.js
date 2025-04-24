dc7d65ff3c3eb79fcd7e2cc35f111802011a1cea
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-25 01:57:22 +0300

    Update 10.Orbit.js

--- JSCoreJSFundamentals/04ArraysAndMatricesExercises/10.Orbit.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesExercises/10.Orbit.js
@@ -1,17 +1,18 @@
-function showOrbit(arr){
-    let cols = arr[0];
-    let rows = arr[1];
+function showOrbit(arr) {
+    let colsCount = arr[0];
+    let rowsCount = arr[1];
     let starRow = arr[2];
     let starCol = arr[3];
+    
     let screenMatrix = [];
-    for (let row = 0; row < rows; row++) {
+    for (let row = 0; row < rowsCount; row++) {
         screenMatrix[row] = [];
-        for (let col = 0; col < cols; col++) {
+        for (let col = 0; col < colsCount; col++) {
             screenMatrix[row][col] = Math.max(Math.abs(row - starRow), Math.abs(col - starCol)) + 1;            
         }        
     }
 
-    for(let row of screenMatrix){
+    for (let row of screenMatrix) {
         console.log(row.join(' '));
     }
 }
