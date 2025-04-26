916e1c63d800313bdb7ebdce2d84624e458d701c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-26 12:59:09 +0300

    Update solution.js

--- JSCoreJSFundamentals/04ArraysAndMatricesLab/01SumFirstLast/solution.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesLab/01SumFirstLast/solution.js
@@ -1,13 +1,13 @@
 function solve() {
-  let arrayInput = JSON.parse(document.getElementById("arr").value);  
-  let resultElement = document.getElementById('result');
-  calculate(arrayInput);
+    let inputArray = JSON.parse(document.getElementById("arr").value);  
+    let resultElement = document.getElementById('result');
+    calculate(inputArray);
 
-  function calculate(array){
-    for (let i = 0; i < array.length; i++) {
-      let p = document.createElement('p');
-      p.textContent = `${i} -> ${array[i] * array.length}`;
-      resultElement.appendChild(p);
+    function calculate(array) {
+        for (let i = 0; i < array.length; i++) {
+            let p = document.createElement('p');
+            p.textContent = `${i} -> ${array[i] * array.length}`;
+            resultElement.appendChild(p);
+        }
     }
-  }
 }
