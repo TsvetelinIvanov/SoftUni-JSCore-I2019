49ff0acb81b3fd1f539781fbc892be0b1feacf1a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-26 13:09:08 +0300

    Update solution.js

--- JSCoreJSFundamentals/04ArraysAndMatricesLab/02EvenPositionElement/solution.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesLab/02EvenPositionElement/solution.js
@@ -1,14 +1,14 @@
 function solve() {
-  let inputArray = JSON.parse(document.getElementById('arr').value);
-  let result = [];
-  calculate();
-  document.getElementById('result').innerHTML = result.join(' x ');
+    let inputArray = JSON.parse(document.getElementById('arr').value);
+    let result = [];
+    calculate();
+    document.getElementById('result').innerHTML = result.join(' x ');
 
-  function calculate(){
-    inputArray.forEach((element, index) => {
-      if (index % 2 === 0){
-        result.push(element);
-      }
-    });
-  }
+    function calculate(){
+        inputArray.forEach((element, index) => {
+            if (index % 2 === 0) {
+                result.push(element);
+            }
+        });
+    }
 }
