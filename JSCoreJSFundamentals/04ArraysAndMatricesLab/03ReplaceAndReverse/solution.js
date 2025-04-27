10334cd68f751abbf6fa75b15709845526eb3036
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-27 14:42:18 +0300

    Update solution.js

--- JSCoreJSFundamentals/04ArraysAndMatricesLab/03ReplaceAndReverse/solution.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesLab/03ReplaceAndReverse/solution.js
@@ -1,14 +1,14 @@
 function solve() {
-  let inputArray = JSON.parse(document.getElementById('arr').value);
-  let result = reverseAndMakeFirstLetterUpper(inputArray);
-  document.getElementById('result').innerHTML = result;
+    let inputArray = JSON.parse(document.getElementById('arr').value);
+    let result = reverseAndMakeFirstLetterUpper(inputArray);
+    document.getElementById('result').innerHTML = result;
 
-  function reverseAndMakeFirstLetterUpper(inputArray){
-    inputArray.forEach((element, index) => {
-      element = element.split('').reverse().join('');
-      inputArray[index] = element.charAt(0).toUpperCase().concat(element.slice(1));
-    });
+    function reverseAndMakeFirstLetterUpper(inputArray) {
+        inputArray.forEach((element, index) => {
+            element = element.split('').reverse().join('');
+            inputArray[index] = element.charAt(0).toUpperCase().concat(element.slice(1));
+        });
 
-    return inputArray.join(' ');
-  }
+        return inputArray.join(' ');
+    }
 }
