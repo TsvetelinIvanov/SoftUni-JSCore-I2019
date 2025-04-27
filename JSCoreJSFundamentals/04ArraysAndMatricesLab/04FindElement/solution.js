e50fe0d0a13a223131b70d9798f48b3a03cda603
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-27 14:50:03 +0300

    Update solution.js

--- JSCoreJSFundamentals/04ArraysAndMatricesLab/04FindElement/solution.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesLab/04FindElement/solution.js
@@ -1,17 +1,17 @@
 function solve() {
-  let number = Number(document.getElementById('num').value);
-  let inputArray = JSON.parse(document.getElementById('arr').value);
-  let answer = findIfSearchedExist(number, inputArray);
-  document.getElementById('result').innerHTML = answer;
+    let number = Number(document.getElementById('num').value);
+    let inputArray = JSON.parse(document.getElementById('arr').value);
+    let answer = findIfSearchedExist(number, inputArray);
+    document.getElementById('result').innerHTML = answer;
   
-  function findIfSearchedExist(searched, input){
-    let result = [];
-    for(let element of input){
-      let isSearchedExist = element.includes(searched);
-      let index = element.indexOf(searched);
-      result.push(isSearchedExist + ' -> ' + index);
-    }
+    function findIfSearchedExist(searched, input) {
+        let result = [];
+        for (let element of input) {
+            let doesSearchedExist = element.includes(searched);
+            let index = element.indexOf(searched);
+            result.push(doesSearchedExist + ' -> ' + index);
+        }
 
-    return result;
-  }
+        return result;
+    }
 }
