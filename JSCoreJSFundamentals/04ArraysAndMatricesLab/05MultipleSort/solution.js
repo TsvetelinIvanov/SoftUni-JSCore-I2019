cd6dbc52bc006ee22cbec3a6aa5cb2325921c95a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-04-28 17:38:11 +0300

    Update solution.js

--- JSCoreJSFundamentals/04ArraysAndMatricesLab/05MultipleSort/solution.js
+++ JSCoreJSFundamentals/04ArraysAndMatricesLab/05MultipleSort/solution.js
@@ -1,17 +1,18 @@
 function solve() {
-  let inputArray = JSON.parse(document.getElementById('arr').value);
-  sortAscendingAndAlphabetically(inputArray);
+    let inputArray = JSON.parse(document.getElementById('arr').value);
+    sortAscendingAndAlphabetically(inputArray);
 
-  function sortAscendingAndAlphabetically(input){
-    let resultElement = document.getElementById('result');    
-    let sortedAscending = input.sort((a, b) => a - b);
-    let div1 = document.createElement('div');
-    div1.textContent = sortedAscending.join(', ');
-    resultElement.appendChild(div1);
-    let sortedAlphabetically = input.sort((a, b) => a.localeCompare(b));
-    //let sortedAlphabetically = input.sort();
-    let div2 = document.createElement('div');
-    div2.textContent = sortedAlphabetically.join(', ');
-    resultElement.appendChild(div2);
-  }
+    function sortAscendingAndAlphabetically(input) {
+        let resultElement = document.getElementById('result');    
+        let sortedAscending = input.sort((a, b) => a - b);
+        let div1 = document.createElement('div');
+        div1.textContent = sortedAscending.join(', ');
+        resultElement.appendChild(div1);
+      
+        let sortedAlphabetically = input.sort((a, b) => a.localeCompare(b));
+        //let sortedAlphabetically = input.sort();
+        let div2 = document.createElement('div');
+        div2.textContent = sortedAlphabetically.join(', ');
+        resultElement.appendChild(div2);
+    }
 }
