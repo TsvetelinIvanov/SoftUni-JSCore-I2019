6e86bb0e44e2bcdf4bf637494d7669dd84bae9ec
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-03 11:05:44 +0300

    Update solution.js

--- JSCoreJSFundamentals/05StringsAndRegExLab/01PascalOrCamelCase/solution.js
+++ JSCoreJSFundamentals/05StringsAndRegExLab/01PascalOrCamelCase/solution.js
@@ -1,30 +1,36 @@
 function solve() {
-  let input = document.getElementById('str1').value;
-  let currentCase = document.getElementById('str2').value;  
-  doPasscalOrCamelCase(input, currentCase);  
+    let input = document.getElementById('str1').value;
+    let currentCase = document.getElementById('str2').value;  
+    doPasscalOrCamelCase(input, currentCase);  
 
-  function doPasscalOrCamelCase(input, currentCase){
-    let splitedInput = input.toLowerCase().split(' ').filter(w => w !== '');  
-    let output = '';
-    if (currentCase === 'Pascal Case'){
-      for(let word of splitedInput){
-        if (word[0] !== word[0].toUpperCase()){
-          word = word.replace(word[0], word[0].toUpperCase());
+    function doPasscalOrCamelCase(input, currentCase) {
+        let splitedInput = input.toLowerCase().split(' ').filter(w => w !== '');  
+        let output = '';
+        if (currentCase === 'Pascal Case') {
+            for (let word of splitedInput) {
+                if (word[0] !== word[0].toUpperCase()) {
+                    word = word.replace(word[0], word[0].toUpperCase());
+                }
+              
+                 output += word;
+            }
+         }
+         else if (currentCase === 'Camel Case') {
+             for (let word of splitedInput) {
+                 if (word[0] !== word[0].toUpperCase()) {
+                     word = word.replace(word[0], word[0].toUpperCase());
+                 }
+               
+                 output += word;
+             }
+           
+             output = output.replace(output[0], output[0].toLowerCase())
         }
-        output += word;
-      }
-    } else if (currentCase === 'Camel Case'){
-      for(let word of splitedInput){
-        if (word[0] !== word[0].toUpperCase()){
-          word = word.replace(word[0], word[0].toUpperCase());
+        else {
+            output = 'Error!'
         }
-        output += word;
-      }
-      output = output.replace(output[0], output[0].toLowerCase())
-    } else {
-      output = 'Error!'
+      
+        console.log(output)
+        document.getElementById('result').innerHTML = output;
     }
-    console.log(output)
-    document.getElementById('result').innerHTML = output;
-  }
 }
