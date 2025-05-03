019024cddc39e81db878d4dfe471316429b53e8e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-04 02:02:27 +0300

    Update solution.js

--- JSCoreJSFundamentals/05StringsAndRegExLab/03SplitStringEqually/solution.js
+++ JSCoreJSFundamentals/05StringsAndRegExLab/03SplitStringEqually/solution.js
@@ -1,27 +1,28 @@
 function solve() {
-  let string = document.getElementById('str').value;
-  let number = Number(document.getElementById('num').value);
-  splitStringEqually(string, number);
+    let string = document.getElementById('str').value;
+    let number = Number(document.getElementById('num').value);
+    splitStringEqually(string, number);
 
-  function splitStringEqually(string, number){    
-    let outputArray = [];    
-    if (string.length % number !== 0){
-      let stringLength = string.length;
-      let symbolsCount = 0;
-      while(stringLength % number !== 0){
-        stringLength %= number;
-        stringLength++;
-        symbolsCount++;
-      }
+    function splitStringEqually(string, number) {    
+        let outputArray = [];    
+        if (string.length % number !== 0) {
+            let stringLength = string.length;
+            let symbolsCount = 0;
+            while (stringLength % number !== 0) {
+                stringLength %= number;
+                stringLength++;
+                symbolsCount++;
+            }
 
-      for (let i = 0; i < symbolsCount; i++) {
-        string +=string[i];                
-      }
+            for (let i = 0; i < symbolsCount; i++) {
+                string +=string[i];                
+            }
+        }
+
+        for (let i = 0; i < string.length; i += number) {
+            outputArray.push(string.substr(i, number));      
+        }
+      
+        document.getElementById('result').innerHTML = outputArray.join(' ');
     }
-
-    for (let i = 0; i < string.length; i += number) {
-      outputArray.push(string.substr(i, number));      
-    }
-    document.getElementById('result').innerHTML = outputArray.join(' ');
-  }
 }
