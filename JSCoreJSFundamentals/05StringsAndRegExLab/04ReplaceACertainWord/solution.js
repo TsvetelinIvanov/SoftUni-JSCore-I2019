1e249828ffc07245d55177d741d899d1f5500796
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-04 02:10:55 +0300

    Update solution.js

--- JSCoreJSFundamentals/05StringsAndRegExLab/04ReplaceACertainWord/solution.js
+++ JSCoreJSFundamentals/05StringsAndRegExLab/04ReplaceACertainWord/solution.js
@@ -1,17 +1,17 @@
 function solve() {
-  let inputArray = JSON.parse(document.getElementById('arr').value);
-  let wordToReplacing = document.getElementById('str').value;
-  replaceWord(inputArray, wordToReplacing);
+    let inputArray = JSON.parse(document.getElementById('arr').value);
+    let wordForReplacing = document.getElementById('str').value;
+    replaceWord(inputArray, wordForReplacing);
 
-  function replaceWord(arr, replacement){
-    let resultElement = document.getElementById('result');
-    let wordToReplaced = arr[0].split(' ').filter(w => w !== '')[2];
-    let regex = new RegExp(wordToReplaced, 'gi');
-    for (let element of arr){
-      element = element.replace(regex, replacement);
-      let p = document.createElement('p');
-      p.textContent = element;
-      resultElement.appendChild(p);
+    function replaceWord(arr, replacement) {
+        let resultElement = document.getElementById('result');
+        let wordReplaced = arr[0].split(' ').filter(w => w !== '')[2];
+        let regex = new RegExp(wordReplaced, 'gi');
+        for (let element of arr) {
+            element = element.replace(regex, replacement);
+            let p = document.createElement('p');
+            p.textContent = element;
+            resultElement.appendChild(p);
+        }
     }
-  }
 }
