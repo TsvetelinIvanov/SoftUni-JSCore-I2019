76cf012413827d515d1900d99c36211b4bc2120c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-13 12:04:17 +0300

    Update 01TownsToJSON.js

--- JSCoreJSFundamentals/06ObjectsAndJSONLab/01TownsToJSON.js
+++ JSCoreJSFundamentals/06ObjectsAndJSONLab/01TownsToJSON.js
@@ -1,4 +1,4 @@
-function doTableToObjects(stringArray){
+function doTableToObjects(stringArray) {
     let objectArray = [];
     let headings = stringArray[0].split('|').filter(h => h !== '').map(h => h.trim());
     for (let i = 1; i < stringArray.length; i++) {
@@ -7,17 +7,17 @@ function doTableToObjects(stringArray){
         town[headings[0]] = townData[0];
         let latitude; 
         let longitude;
-        //if (townData[0] === 'Veliko Turnovo' || townData[0] === 'New York'){
-            if (townData[0].split(' ').length === 2){
+        //if (townData[0] === 'Veliko Turnovo' || townData[0] === 'New York') {
+        if (townData[0].split(' ').length === 2) {
             latitude = Number(townData[1]);
             longitude = Number(townData[2]);            
         }
-        else{
+        else {
             latitude = Number(Number(townData[1]).toFixed(2));
             longitude = Number(Number(townData[2]).toFixed(2));
-        }       
-        town[headings[1]] = latitude;
+        }
         
+        town[headings[1]] = latitude;
         town[headings[2]] = longitude;
         objectArray.push(town);       
     }
@@ -26,5 +26,4 @@ function doTableToObjects(stringArray){
 }
 
 doTableToObjects(['| Town | Latitude | Longitude |', '| Sofia | 42.696552 | 23.32601 |', '| Beijing | 39.913818 | 116.363625 |'])
-doTableToObjects(['| Town | Latitude | Longitude |', '| Veliko Turnovo | 43.0757 | 25.6172 |', '| Monatevideo | 34.50 | 56.11 |']
-)
+doTableToObjects(['| Town | Latitude | Longitude |', '| Veliko Turnovo | 43.0757 | 25.6172 |', '| Monatevideo | 34.50 | 56.11 |'])
