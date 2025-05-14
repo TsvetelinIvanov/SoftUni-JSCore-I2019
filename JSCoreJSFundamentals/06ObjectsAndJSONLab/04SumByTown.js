ee450896f4ace8844b9662ae5175623f6f37b223
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-14 23:07:02 +0300

    Update 04SumByTown.js

--- JSCoreJSFundamentals/06ObjectsAndJSONLab/04SumByTown.js
+++ JSCoreJSFundamentals/06ObjectsAndJSONLab/04SumByTown.js
@@ -1,12 +1,13 @@
-function sumTownsIncome(stringArray){    
+function sumTownsIncome(stringArray) {    
     let townIncomes = {};
     for (let i = 0; i < stringArray.length; i += 2) {
         let town = stringArray[i];
         let income = Number(stringArray[i + 1]);
-        //if (!(town in townIncomes)){
-        if (!townIncomes.hasOwnProperty(town)){
+        //if (!(town in townIncomes)) {
+        if (!townIncomes.hasOwnProperty(town)) {
             townIncomes[town] = 0;
         }
+        
         townIncomes[town] += income;
     }
 
