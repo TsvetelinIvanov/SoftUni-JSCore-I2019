58ce148946eefab0b0ac3515ad875f23d9963c99
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-15 12:49:01 +0300

    Update 09LowestPricesInCities.js

--- JSCoreJSFundamentals/06ObjectsAndJSONLab/09LowestPricesInCities.js
+++ JSCoreJSFundamentals/06ObjectsAndJSONLab/09LowestPricesInCities.js
@@ -1,4 +1,4 @@
-function findLowestPrice(priceArray) {
+function findTheLowestPrice(priceArray) {
     let products = {};
     for (let priceData of priceArray) {
         let [townName, productName, price] = priceData.split(' | ');
@@ -29,5 +29,5 @@ function findLowestPrice(priceArray) {
     console.log(lowestPrices.join('\n'));
 }
 
-findLowestPrice(['Sample Town | Sample Product | 1000', 'Sample Town | Orange | 2', 'Sample Town | Peach | 1', 'Sofia | Orange | 3', 'Sofia | Peach | 2', 'New York | Sample Product | 1000.1', 'New York | Burger | 10'])
-findLowestPrice(['Sofia City | Audi | 100000', 'Sofia City | BMW | 100000', 'Sofia City | Mitsubishi | 10000', 'Sofia City | Mercedes | 10000', 'Sofia City | NoOffenseToCarLovers | 0', 'Mexico City | Audi | 1000', 'Mexico City | BMW | 99999', 'New York City | Mitsubishi | 10000', 'New York City | Mitsubishi | 1000', 'Mexico City | Audi | 100000', 'Washington City | Mercedes | 1000'])
+findTheLowestPrice(['Sample Town | Sample Product | 1000', 'Sample Town | Orange | 2', 'Sample Town | Peach | 1', 'Sofia | Orange | 3', 'Sofia | Peach | 2', 'New York | Sample Product | 1000.1', 'New York | Burger | 10'])
+findTheLowestPrice(['Sofia City | Audi | 100000', 'Sofia City | BMW | 100000', 'Sofia City | Mitsubishi | 10000', 'Sofia City | Mercedes | 10000', 'Sofia City | NoOffenseToCarLovers | 0', 'Mexico City | Audi | 1000', 'Mexico City | BMW | 99999', 'New York City | Mitsubishi | 10000', 'New York City | Mitsubishi | 1000', 'Mexico City | Audi | 100000', 'Washington City | Mercedes | 1000'])
