eb63eb13193168a974e34287eaf701d43d9e319c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-17 15:04:10 +0300

    Update 01HTMLGrade.js

--- JSCoreJSFundamentals/ExamJSFundamentals10II2019/01HTMLGrade.js
+++ JSCoreJSFundamentals/ExamJSFundamentals10II2019/01HTMLGrade.js
@@ -1,16 +1,17 @@
-function calculateGrade(examPoints, homeworkPoints, totalHomework){
+function calculateGrade(examPoints, homeworkPoints, totalHomework) {
     let totalExamPoits = (examPoints * 100 /400) * 0.9 ;
     let totalHomeworkPoits = (homeworkPoints * 100 / totalHomework) * 0.1;
     let totalPoints = totalExamPoits + totalHomeworkPoits;
 
     let grade = 3 + 2 * (totalPoints - 100 / 5) / (100 / 2);
-    if (examPoints === 400){
+    if (examPoints === 400) {
         grade = 6;
     }
 
-    if(grade < 3){
+    if(grade < 3) {
         grade = 2;
-    } else if (grade >= 6){
+    }
+    else if (grade >= 6) {
         grade = 6;
     }
 
