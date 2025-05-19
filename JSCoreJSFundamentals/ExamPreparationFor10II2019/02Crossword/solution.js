016403fa0b5ef98d81eb8a5db2a67219178784f1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-19 07:46:15 +0300

    Update solution.js

--- JSCoreJSFundamentals/ExamPreparationFor10II2019/02Crossword/solution.js
+++ JSCoreJSFundamentals/ExamPreparationFor10II2019/02Crossword/solution.js
@@ -13,58 +13,61 @@ function solve() {
    rotateButton.addEventListener('click', rotate);
    getButton.addEventListener('click', get);
 
-   function filter(){
-      let inputSplited = inputElement.value.split('');      
-      //let inputSplited = document.getElementById('input').value.split('');      
+   function filter() {
+      let splittedInput = inputElement.value.split('');      
+      //let splittedInput = document.getElementById('input').value.split('');      
       let secondCommand = document.getElementById('filterSecondaryCmd').value;
       let indexPosition = Number(document.getElementById('filterPosition').value) - 1;
-      switch(secondCommand){
-         //case 'UPERCASE': 
+      switch(secondCommand) {
+         //case 'UPPERCASE': 
          case 'uppercase': 
-         //outputP.textContent += inputSplited.filter(c => c === c.toUpperCase() && !Number(c))[indexPosition];
-         outputP.textContent += inputSplited.filter(c => c === c.toUpperCase() && isNaN(c))[indexPosition];
+         //outputP.textContent += splittedInput.filter(c => c === c.toUpperCase() && !Number(c))[indexPosition];
+         outputP.textContent += splittedInput.filter(c => c === c.toUpperCase() && isNaN(c))[indexPosition];
          break;
          //case 'LOWERCASE':
          case 'lowercase':
-         //outputP.textContent += inputSplited.filter(c => c === c.toLowerCase() && !Number(c))[indexPosition];
-         outputP.textContent += inputSplited.filter(c => c === c.toLowerCase() && isNaN(c))[indexPosition];
+         //outputP.textContent += splittedInput.filter(c => c === c.toLowerCase() && !Number(c))[indexPosition];
+         outputP.textContent += splittedInput.filter(c => c === c.toLowerCase() && isNaN(c))[indexPosition];
          break;
          //case 'NUMS':
          case 'nums':
-         //outputP.textContent += inputSplited.filter(c => Number(c))[indexPosition];
-         outputP.textContent += inputSplited.filter(c => !isNaN(c))[indexPosition];
+         //outputP.textContent += splittedInput.filter(c => Number(c))[indexPosition];
+         outputP.textContent += splittedInput.filter(c => !isNaN(c))[indexPosition];
          break;
       }
    }
 
-   function sort(){
-      //let inputSplited = inputElement.value.split('').sort();
-      //let inputSplited = inputElement.value.split('').sort((a, b) => a.localeCompare(b));
-      let inputSplited = inputElement.value.split('');
+   function sort() {
+      //let splittedInput = inputElement.value.split('').sort();
+      //let splittedInput = inputElement.value.split('').sort((a, b) => a.localeCompare(b));
+      let splittedInput = inputElement.value.split('');
       let secondCommand = document.getElementById('sortSecondaryCmd').value;
       let indexPosition = Number(document.getElementById('sortPosition').value) - 1;
       // if (secondCommand === 'Z'){
-      //    inputSplited = inputSplited.reverse();
+      //    splittedInput = splittedInput.reverse();
       // }
       if (secondCommand === 'A'){
-         inputSplited.sort((a, b) => a.localeCompare(b));
-      } else {
-         inputSplited.sort((a, b) => b.localeCompare(a));
+         splittedInput.sort((a, b) => a.localeCompare(b));
       }
-      outputP.textContent += inputSplited[indexPosition];      
+      else {
+         splittedInput.sort((a, b) => b.localeCompare(a));
+      }
+      
+      outputP.textContent += splittedInput[indexPosition];      
    }
 
-   function rotate(){
-      let inputSplited = inputElement.value.split('');
+   function rotate() {
+      let splittedInput = inputElement.value.split('');
       let secondCommand = document.getElementById('rotateSecondaryCmd').value;
       let indexPosition = Number(document.getElementById('rotatePosition').value) - 1;
-      for (let i = 0; i < secondCommand % inputSplited.length; i++) {
-         inputSplited.unshift(inputSplited.pop());
+      for (let i = 0; i < secondCommand % splittedInput.length; i++) {
+         splittedInput.unshift(splittedInput.pop());
       }
-      outputP.textContent += inputSplited[indexPosition];
+      
+      outputP.textContent += splittedInput[indexPosition];
    }
 
-   function get(){      
+   function get() {      
       //let input = inputElement.value.split('');
       let input = inputElement.value;
       let indexPosition = Number(document.getElementById('getPosition').value) - 1;
