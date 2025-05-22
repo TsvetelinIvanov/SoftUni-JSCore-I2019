87a5668f0c8e6ee5079387a3054213ed4cf6eb1c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-22 19:47:58 +0300

    Update 01SortArray.js

--- JSCoreAdvanced/01AdvancedFunctionsExercises/01SortArray.js
+++ JSCoreAdvanced/01AdvancedFunctionsExercises/01SortArray.js
@@ -1,15 +1,15 @@
-function sortArray(inputArray, sortOrder){
-    // if(sortOrder === 'asc'){
+function sortArray(inputArray, sortOrder) {
+    // if (sortOrder === 'asc') {
     //     return inputArray.sort((a, b) => a - b);
-    // } else if(sortOrder === 'desc'){
+    // } else if (sortOrder === 'desc') {
     //     return inputArray.sort((a, b) => b - a);
     // }
 
-    let ascendingComparator = function (a, b){
+    let ascendingComparator = function (a, b) {
         return a - b;
     }
 
-    let descendingComparator = function (a, b){
+    let descendingComparator = function (a, b) {
         return b - a;
     }
 
