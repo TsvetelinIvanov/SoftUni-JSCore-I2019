f92d6eb0f485df86a74312650601b485df54b8dc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-24 15:11:25 +0300

    Update 07MonkeyPatcher.js

--- JSCoreAdvanced/01AdvancedFunctionsExercises/07MonkeyPatcher.js
+++ JSCoreAdvanced/01AdvancedFunctionsExercises/07MonkeyPatcher.js
@@ -1,31 +1,35 @@
-function monkeyPatch(commandName){
+function monkeyPatch(commandName) {
     let balance = this.upvotes - this.downvotes;
     let totalVoteCount = this.upvotes + this.downvotes;    
     let commands = {
-        upvote: () =>this.upvotes++,
+        upvote: () => this.upvotes++,
         downvote: () => this.downvotes++,
         score: () => {
             let upvotesToReport = this.upvotes;
             let downvotesToReport = this.downvotes;
-            if(totalVoteCount > 50){
+            if (totalVoteCount > 50) {
                 let obfuscatedVotes = Math.ceil(Math.max(upvotesToReport, downvotesToReport) * 0.25);
                 upvotesToReport += obfuscatedVotes;                                
                 downvotesToReport += obfuscatedVotes;                                
             }
+            
             return [upvotesToReport, downvotesToReport, balance, getRating.call(this)];
 
             function getRating() {
-                if(totalVoteCount < 10){
+                if (totalVoteCount < 10) {
                     return 'new';
                 }
                 
-                if(this.upvotes > totalVoteCount * 0.66){
+                if (this.upvotes > totalVoteCount * 0.66) {
                     return 'hot';
-                } else if(balance >= 0 && this.upvotes > 100){                    
+                }
+                else if (balance >= 0 && this.upvotes > 100) {                    
                     return 'controversial';
-                } else if(balance < 0){
+                }
+                else if (balance < 0) {
                     return 'unpopular';
-                } else {
+                }
+                else {
                     return 'new';
                 }
             }
@@ -49,4 +53,5 @@ console.log(monkeyPatch.call(post, 'score'))
 for (let i = 0; i < 50; i++) {
     monkeyPatch.call(post, 'downvote')    
 }
+
 console.log(monkeyPatch.call(post, 'score'))
