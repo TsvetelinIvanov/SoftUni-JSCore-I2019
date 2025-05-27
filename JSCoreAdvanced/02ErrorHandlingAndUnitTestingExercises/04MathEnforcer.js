13acc59783d8f6326db7468fb8b9d512a7105e55
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-27 12:49:20 +0300

    Update 04MathEnforcer.js

--- JSCoreAdvanced/02ErrorHandlingAndUnitTestingExercises/04MathEnforcer.js
+++ JSCoreAdvanced/02ErrorHandlingAndUnitTestingExercises/04MathEnforcer.js
@@ -3,18 +3,21 @@ let mathEnforcer = {
         if (typeof(num) !== 'number') {
             return undefined;
         }
+        
         return num + 5;
     },
     subtractTen: function (num) {
         if (typeof(num) !== 'number') {
             return undefined;
         }
+        
         return num - 10;
     },
     sum: function (num1, num2) {
         if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
             return undefined;
         }
+        
         return num1 + num2;
     }
 };
