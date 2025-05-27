3f1fb16740dafa990e98a6a4f3123c2a5632bdcd
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-27 12:47:00 +0300

    Update 01RequestValidator.js

--- JSCoreAdvanced/02ErrorHandlingAndUnitTestingExercises/01RequestValidator.js
+++ JSCoreAdvanced/02ErrorHandlingAndUnitTestingExercises/01RequestValidator.js
@@ -1,30 +1,30 @@
-function validateRequest(request){
+function validateRequest(request) {
     let methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
-    let uriPattern = /^[\w.]+$/;
     let versions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
+    let uriPattern = /^[\w.]+$/;
     let messagePattern = /^[^<>\\&'"]*$/g;    
 
     let headerKeys = Object.keys(request);
-    if(!headerKeys.includes('method') || !methods.includes(request.method)){
+    if (!headerKeys.includes('method') || !methods.includes(request.method)) {
         throw new Error('Invalid request header: Invalid Method');
     }
 
-    if(!headerKeys.includes('uri') || !uriPattern.test(request.uri)){
+    if (!headerKeys.includes('uri') || !uriPattern.test(request.uri)) {
         throw new Error('Invalid request header: Invalid URI');
     }
 
-    if(!headerKeys.includes('version') || !versions.includes(request.version)){
+    if (!headerKeys.includes('version') || !versions.includes(request.version)) {
         throw new Error('Invalid request header: Invalid Version');
     }
 
-    if(!headerKeys.includes('message') || !messagePattern.test(request.message)){
+    if (!headerKeys.includes('message') || !messagePattern.test(request.message)) {
         throw new Error('Invalid request header: Invalid Message');
     }
 
     return request;
 }
 
-console.log(validateRequest({ method: 'GET', uri: 'svn.public.catalog', version: 'HTTP/1.1', message: ''}));  
+console.log(validateRequest({method: 'GET', uri: 'svn.public.catalog', version: 'HTTP/1.1', message: ''}));  
 
 validateRequest({
     method: 'OPTIONS',
@@ -37,5 +37,4 @@ validateRequest({
     method: 'POST',
     uri: 'home.bash',
     message: 'rm -rf /*'
-  });
-  
+  });  
