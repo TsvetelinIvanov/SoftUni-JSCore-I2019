fbb800555d840d0003f9b514c32d9d72abc6e67b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-05-28 17:22:22 +0300

    Update 04SumOfNumbers-tests.js

--- JSCoreAdvanced/02ErrorHandlingAndUnitTestingWithMochaLab/test/04SumOfNumbers-tests.js
+++ JSCoreAdvanced/02ErrorHandlingAndUnitTestingWithMochaLab/test/04SumOfNumbers-tests.js
@@ -5,25 +5,25 @@ describe('sum(arr) - sum array of numbers', function() {
     it('should return 3 for [1, 2]', function() {
         expect(sum([1, 2])).to.be.equal(3);
     });
-    it('should return 100101 for [1, 100000, 100]', function(){
+    it('should return 100101 for [1, 100000, 100]', function() {
         expect(sum([1, 100000, 100])).to.be.equal(100101);
     });
-    it('should return -7 for [10, 0, -17]', function(){
+    it('should return -7 for [10, 0, -17]', function() {
         expect(sum([10, 0, -17])).to.be.equal(-7);
     });
-    it('should return 1.7 for [1, 0, 0.7]', function(){
+    it('should return 1.7 for [1, 0, 0.7]', function() {
         expect(sum([1, 0, 0.7])).to.be.equal(1.7);
     });
-    it('should return 10 for [10]', function(){
+    it('should return 10 for [10]', function() {
         expect(sum([10])).to.be.equal(10);
     });
-    it('should return 0 for empty array', function(){
+    it('should return 0 for empty array', function() {
         expect(sum([])).to.be.equal(0);
     });
-    it('should return NaN for invalid data', function(){
+    it('should return NaN for invalid data', function() {
         expect(sum(['invalid data'])).to.be.NaN;
     });
-    it('should return NaN for not array', function(){
+    it('should return NaN for no array', function() {
         expect(sum('invalid data')).to.be.NaN;
     });
 });
