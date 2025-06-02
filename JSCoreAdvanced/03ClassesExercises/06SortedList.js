04a626e94a392fc65d0ae37cc2e2eabf2e8919a4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-02 06:04:19 +0300

    Update 06SortedList.js

--- JSCoreAdvanced/03ClassesExercises/06SortedList.js
+++ JSCoreAdvanced/03ClassesExercises/06SortedList.js
@@ -1,29 +1,31 @@
-class SortedList{
-    constructor(){
+class SortedList {
+    constructor() {
         this.sortedList = [];
         this.size = 0;
     }
 
-    add(element){
+    add(element) {
         this.sortedList.push(element);
         this.sortedList.sort((a, b) => a - b);
         this.size++;
     }
 
-    remove(index){
-        if(index < 0 || index >= this.size){
+    remove(index) {
+        if (index < 0 || index >= this.size) {
             //return;
             throw new Error('Incorrect index!');
         }
+        
         this.sortedList.splice(index, 1);
         this.size--;
     }
 
-    get(index){
-        if(index < 0 || index >= this.size){
+    get(index) {
+        if (index < 0 || index >= this.size) {
             //return;
             throw new Error('Incorrect index!');
         }
+        
         return this.sortedList[index];
     }
 }
