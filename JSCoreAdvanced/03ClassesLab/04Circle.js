41231132ee69e3a1ffd43e6e3b3d63251bae3460
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-05 20:28:18 +0300

    Update 04Circle.js

--- JSCoreAdvanced/03ClassesLab/04Circle.js
+++ JSCoreAdvanced/03ClassesLab/04Circle.js
@@ -1,26 +1,26 @@
-class Circle{
-    constructor(radius){
+class Circle {
+    constructor(radius) {
         this.radius = radius
     }
 
-    get diameter(){
+    get diameter() {
         return 2 * this.radius;
     }
 
-    set diameter(diameter){
+    set diameter(diameter) {
         this.radius = diameter / 2;
     }
 
-    get area(){
+    get area() {
         return Math.PI * this.radius ** 2;
     }
 }
 
-let c = new Circle(2);
-console.log(`Radius: ${c.radius}`);
-console.log(`Diameter: ${c.diameter}`);
-console.log(`Area: ${c.area}`);
-c.diameter = 1.6;
-console.log(`Radius: ${c.radius}`);
-console.log(`Diameter: ${c.diameter}`);
-console.log(`Area: ${c.area}`);
+let circle = new Circle(2);
+console.log(`Radius: ${circle.radius}`);
+console.log(`Diameter: ${circle.diameter}`);
+console.log(`Area: ${circle.area}`);
+circle.diameter = 1.6;
+console.log(`Radius: ${circle.radius}`);
+console.log(`Diameter: ${circle.diameter}`);
+console.log(`Area: ${circle.area}`);
