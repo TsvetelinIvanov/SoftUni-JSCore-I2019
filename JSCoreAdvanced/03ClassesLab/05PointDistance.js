4f0820a1eeb958bde3ef068965eeb000a959e99a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-05 20:30:05 +0300

    Update 05PointDistance.js

--- JSCoreAdvanced/03ClassesLab/05PointDistance.js
+++ JSCoreAdvanced/03ClassesLab/05PointDistance.js
@@ -1,10 +1,10 @@
-class Point{
-    constructor(x, y){
+class Point {
+    constructor(x, y) {
         this.x = x,
         this.y = y
     }
 
-    static distance(point1, point2){
+    static distance(point1, point2) {
         return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2);
     }
 }
