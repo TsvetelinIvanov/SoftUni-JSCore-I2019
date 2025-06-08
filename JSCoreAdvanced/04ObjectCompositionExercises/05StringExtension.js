4dc6c9bd1c96afd6d1d77caaa23574e3fe1d4b8e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-08 22:58:31 +0300

    Update 05StringExtension.js

--- JSCoreAdvanced/04ObjectCompositionExercises/05StringExtension.js
+++ JSCoreAdvanced/04ObjectCompositionExercises/05StringExtension.js
@@ -1,5 +1,5 @@
 (function extendString() {
-    String.prototype.ensureStart = function(str){
+    String.prototype.ensureStart = function(str) {
         let stringToReturn = this.toString();
         if (!stringToReturn.startsWith(str)) {
             stringToReturn = str + stringToReturn;
@@ -8,49 +8,51 @@
         return stringToReturn;
     };
 
-    String.prototype.ensureEnd = function(str){
+    String.prototype.ensureEnd = function(str) {
         let stringToReturn = this.toString();
-        if(!stringToReturn.endsWith(str)){
+        if (!stringToReturn.endsWith(str)) {
             stringToReturn += str;
         }
 
         return stringToReturn;
     };
 
-    String.prototype.isEmpty = function(){
-        if(this.length === 0){
+    String.prototype.isEmpty = function() {
+        if (this.length === 0) {
             return true;
         }
+        
         return false;
     };
 
-    String.prototype.truncate = function(n){
+    String.prototype.truncate = function(n) {
         let stringToReturn = this.toString();
-        if(n < 4){
+        if (n < 4) {
             return '.'.repeat(n);
         }
 
-        if(stringToReturn.length <= n){
+        if (stringToReturn.length <= n) {
             return stringToReturn;
         }
 
         stringToReturn = stringToReturn.substr(0, n - 2);
         let lastWhitespaceIndex = stringToReturn.lastIndexOf(' ');
-        if(lastWhitespaceIndex === -1){
+        if (lastWhitespaceIndex === -1) {
             return stringToReturn.substr(0, stringToReturn.length - 1) + '...';
         }
 
         return stringToReturn.substr(0, lastWhitespaceIndex) + '...';
     };
 
-    String.format = function(string, ...params){
+    String.format = function(string, ...params) {
         for (let i = 0; i < params.length; i++) {
             let index = string.indexOf(`{${i}}`);
-            while(index !== -1){
+            while (index !== -1) {
                 string = string.replace(`{${i}}`, params[i]);
                 index = string.indexOf(`{${i}}`);
             }                       
         }
+        
         return string;
     };
 })()
@@ -70,9 +72,7 @@ str = str.truncate(4)
 console.log(str)
 str = str.truncate(2)
 console.log(str)
-str = String.format('The {0} {1} fox',
-    'quick', 'brown');
+str = String.format('The {0} {1} fox', 'quick', 'brown');
 console.log(str)
-str = String.format('jumps {0} {1}',
-    'dog');
+str = String.format('jumps {0} {1}', 'dog');
 console.log(str)
