fe56a6df872819606517976c72a8feac2bd00434
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-09 23:56:16 +0300

    Update 06SortedList.js

--- JSCoreAdvanced/04ObjectCompositionExercises/06SortedList.js
+++ JSCoreAdvanced/04ObjectCompositionExercises/06SortedList.js
@@ -1,30 +1,39 @@
-function manipulateSortedList(){
-    let processSortedList = (function(){
+function manipulateSortedList() {
+    let processSortedList = (function() {
         let sortedList = [];
-        let add = function(element){
+        let add = function(element) {
             sortedList.push(element);
             sortedList.sort((a, b) => a - b);
             this.size++;
+            
             return sortedList;
         };
-        let remove = function(index){
-            if(index < 0 || index >= this.size){
+        
+        let remove = function(index) {
+            if (index < 0 || index >= this.size){
                 //return;
                 throw new Error('Incorrect index!')
             }
+            
             sortedList.splice(index, 1);
             this.size--;
+            
             return sortedList;
         };
-        let get = function(index){
-            if(index < 0 || index >= this.size){
+        
+        let get = function(index) {
+            if (index < 0 || index >= this.size) {
                 //return;
                 throw new Error('Incorrect index!')
             }
+            
             return sortedList[index];            
         };
+        
         let size = 0;
+        
         return {add, remove, get, size};
     })();
+    
     return processSortedList;
 }
