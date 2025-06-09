cd1cf5c8e4ae1a9f367e517248516e5941685869
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-09 23:58:04 +0300

    Update 07DOMTraversal.js

--- JSCoreAdvanced/04ObjectCompositionExercises/07DOMTraversal.js
+++ JSCoreAdvanced/04ObjectCompositionExercises/07DOMTraversal.js
@@ -1,14 +1,17 @@
-function traverseDom(selector){
+function traverseDom(selector) {
     let $target = $(selector).children();
-    if($target.length === 0){
+    if ($target.length === 0) {
         $(selector).addClass("highlight");
+        
         return;
     }
+    
     let $next = $target;
-    while($next.length > 0){
+    while ($next.length > 0) {
         $target = $next;
         $next = $next.children();
     }
+    
     $target.first().addClass("highlight");
     $target.first().parentsUntil($(selector).parent()).addClass("highlight");
 }
