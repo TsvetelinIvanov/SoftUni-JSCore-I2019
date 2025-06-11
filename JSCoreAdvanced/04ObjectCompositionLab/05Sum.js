e6d4ab93825b2fe77fc21d6aab1dd02d51910e5d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-11 17:18:56 +0300

    Update 05Sum.js

--- JSCoreAdvanced/04ObjectCompositionLab/05Sum.js
+++ JSCoreAdvanced/04ObjectCompositionLab/05Sum.js
@@ -1,19 +1,20 @@
-function addOrSubtract(){
+function addOrSubtract() {
     let elementNum1, elementNum2, elementResult;
-    function init(selectorNum1, selectorNum2, selectorResult){
+    function init(selectorNum1, selectorNum2, selectorResult) {
         elementNum1 = document.querySelector(selectorNum1);
         elementNum2 = document.querySelector(selectorNum2);
         elementResult = document.querySelector(selectorResult);
     }
 
-    function add(){
+    function add() {
         elementResult.value = Number(elementNum1.value) + Number(elementNum2.value);
     }
 
-    function subtract(){
+    function subtract() {
         elementResult.value = Number(elementNum1.value) - Number(elementNum2.value);
     }
 
     let returnedFunctions = {init, add, subtract};
+    
     return returnedFunctions;
 }
