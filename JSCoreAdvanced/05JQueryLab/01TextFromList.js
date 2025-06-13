f5b74bc913830aaa678a680fb426b48b25e3dc40
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-13 11:21:57 +0300

    Update 01TextFromList.js

--- JSCoreAdvanced/05JQueryLab/01TextFromList.js
+++ JSCoreAdvanced/05JQueryLab/01TextFromList.js
@@ -1,8 +1,9 @@
-function extractText (){
+function extractText () {
     let $itemElements = $('#items li');
     let itemElementsArray = $itemElements.toArray();
     let itemTexts = itemElementsArray.map(li => li.textContent);
     let resultText = itemTexts.join(', ');
+    
     let $resultElement = $('#result');
     $resultElement.text(resultText);
 }
