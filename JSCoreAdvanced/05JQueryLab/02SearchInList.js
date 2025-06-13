1c1755aa1ec0513f2181ad97bd3ceada90ae5fff
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-13 11:23:09 +0300

    Update 02SearchInList.js

--- JSCoreAdvanced/05JQueryLab/02SearchInList.js
+++ JSCoreAdvanced/05JQueryLab/02SearchInList.js
@@ -1,9 +1,11 @@
-function search(){
+function search() {
     let searchedText = $('#searchText').val();
     let $matchedElements = $(`#towns li:contains(${searchedText})`)
     $matchedElements.css("font-weight", "bold");
+    
     let $notMatchedElements = $(`#towns li:not(:contains(${searchedText}))`);
     $notMatchedElements.css("font-weight", "normal");
+    
     let $resutElement = $('#result');
     $resutElement.text($matchedElements.length + ' matches found');
 }
