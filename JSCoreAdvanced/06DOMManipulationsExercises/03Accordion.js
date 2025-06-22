34b01363c8ab3964adcf030f3f469b40ce62bc26
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-22 23:03:45 +0300

    Update 03Accordion.js

--- JSCoreAdvanced/06DOMManipulationsExercises/03Accordion.js
+++ JSCoreAdvanced/06DOMManipulationsExercises/03Accordion.js
@@ -1,7 +1,7 @@
 function toggle() {
     let spanButton = document.getElementsByClassName('button')[0];
     let divExtra = document.getElementById('extra');
-    if (spanButton.textContent === 'More'){
+    if (spanButton.textContent === 'More') {
     //if (spanButton.textContent === 'More' || spanButton.style.display === 'none') {
         spanButton.textContent = 'Less';
         divExtra.style.display = 'block';
