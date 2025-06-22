f949e78fb84583930c812a5e4709cd46fdd79e55
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-22 23:28:49 +0300

    Update 04Sections.js

--- JSCoreAdvanced/06DOMManipulationsExercises/04Sections.js
+++ JSCoreAdvanced/06DOMManipulationsExercises/04Sections.js
@@ -1,6 +1,6 @@
-function create(sentences){
+function create(sentences) {
     let divContent = document.getElementById('content');
-    for(let sentence of sentences){
+    for (let sentence of sentences) {
         let p = document.createElement('p');
         p.textContent = sentence;
         p.style.display = 'none';
@@ -10,7 +10,7 @@ function create(sentences){
         divContent.appendChild(div);
     }
 
-    function showParagraph(event){
+    function showParagraph(event) {
         //let p = event.target.firstChild;
         let p = this.firstChild;
         //let p = this.children[0];
