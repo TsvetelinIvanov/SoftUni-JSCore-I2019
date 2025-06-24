232cab03561f3bcbd73041a0ccc21505ffd33b07
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-24 13:00:23 +0300

    Update 01ListOfItems.js

--- JSCoreAdvanced/06DOMManipulationsLab/01ListOfItems.js
+++ JSCoreAdvanced/06DOMManipulationsLab/01ListOfItems.js
@@ -1,4 +1,4 @@
-function addItem(){
+function addItem() {
     let inputElement = document.getElementById('newItemText');
     let li = document.createElement('li');
     li.textContent = inputElement.value;
