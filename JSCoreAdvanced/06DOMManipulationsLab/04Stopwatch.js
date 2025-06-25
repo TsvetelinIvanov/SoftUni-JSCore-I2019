de975e09edff04c27be08987e81ecb4d25ca2bdf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-25 22:56:34 +0300

    Update 04Stopwatch.js

--- JSCoreAdvanced/06DOMManipulationsLab/04Stopwatch.js
+++ JSCoreAdvanced/06DOMManipulationsLab/04Stopwatch.js
@@ -1,4 +1,4 @@
-function stopwatch(){
+function stopwatch() {
     let startBtn = document.getElementById('startBtn');
     let stopBtn = document.getElementById('stopBtn');
     startBtn.addEventListener('click', startTimer);
@@ -7,7 +7,7 @@ function stopwatch(){
     let time = 0;
     let timeInterval;
 
-    function startTimer(){
+    function startTimer() {
         //time = 0;
         document.getElementById('time').textContent = '00:00';
         timeInterval = setInterval(runTime, 1000);
@@ -15,21 +15,21 @@ function stopwatch(){
         stopBtn.disabled = false;
     }
 
-    function stopTimer(){
+    function stopTimer() {
         clearInterval(timeInterval);
         stopBtn.disabled = true;
         startBtn.disabled = false;
     }
     
-    function runTime(){
+    function runTime() {
         time++;
         let seconds = time % 60;
-        if(seconds <= 9){
+        if(seconds <= 9) {
             seconds = '0' + seconds;
         }
 
         let minutes = Math.trunc(time / 60);
-        if(minutes <= 9){
+        if (minutes <= 9) {
             minutes = '0' + minutes; 
         }
 
