715a12d506d289091fa7c957ed6b7e561bab3212
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-25 23:02:47 +0300

    Update 05MouseGradient.js

--- JSCoreAdvanced/06DOMManipulationsLab/05MouseGradient.js
+++ JSCoreAdvanced/06DOMManipulationsLab/05MouseGradient.js
@@ -1,16 +1,16 @@
-function attachGradientEvents(){
+function attachGradientEvents() {
     let gradientElement = document.getElementById('gradient');
     let resultElement = document.getElementById('result');
     gradientElement.addEventListener('mousemove', showGradient);
     gradientElement.addEventListener('mouseout', hideGradient);
 
-    function showGradient(event){
+    function showGradient(event) {
         let gradient = event.offsetX / (event.target.clientWidth - 1);
         gradient = Math.trunc(gradient * 100);
         resultElement.textContent = gradient + '%';
     }
 
-    function hideGradient(){
+    function hideGradient() {
         resultElement.textContent = '';
     }
 }
