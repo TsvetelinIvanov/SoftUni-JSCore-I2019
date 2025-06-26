549a763c05c37b52255ee1a881b7c2687fc1fdbc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-26 06:39:57 +0300

    Update 06HighlightActive.js

--- JSCoreAdvanced/06DOMManipulationsLab/06HighlightActive.js
+++ JSCoreAdvanced/06DOMManipulationsLab/06HighlightActive.js
@@ -1,4 +1,4 @@
-function focus(){
+function focus() {
     let inputElements = document.getElementsByTagName('input');
     Array.from(inputElements).forEach(input => {
         input.addEventListener('focus', event => event.target.parentNode.className = 'focused');
