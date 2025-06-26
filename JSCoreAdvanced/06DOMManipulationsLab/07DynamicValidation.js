1dd4caddbed7dde22fa6f6af2c7d86f27014ce87
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-26 06:42:35 +0300

    Update 07DynamicValidation.js

--- JSCoreAdvanced/06DOMManipulationsLab/07DynamicValidation.js
+++ JSCoreAdvanced/06DOMManipulationsLab/07DynamicValidation.js
@@ -1,11 +1,11 @@
-function validate(){
+function validate() {
     let inputEmail = document.getElementById('email');
     inputEmail.addEventListener('change', validateEmail);
 
-    function validateEmail(){
+    function validateEmail() {
         let emailPattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
         let email = inputEmail.value;
-        if(emailPattern.test(email)){
+        if (emailPattern.test(email)) {
             inputEmail.removeAttribute('class');
         }
         else {
