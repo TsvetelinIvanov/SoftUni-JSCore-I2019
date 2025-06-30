7f63dc818b665f9d1dc461428c21d09d5bc0b8cf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-06-30 12:49:34 +0300

    Update notify.js

--- JSCoreAdvanced/07WorkshopLandingPage/AdditionalRawMaterials-not-direct-in-WorkshopElements/notify.js
+++ JSCoreAdvanced/07WorkshopLandingPage/AdditionalRawMaterials-not-direct-in-WorkshopElements/notify.js
@@ -1,248 +1,273 @@
 /* Notify.js - http://notifyjs.com/ Copyright (c) 2015 MIT */
 (function (factory) {
-	// UMD start
-	// https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
-	} else if (typeof module === 'object' && module.exports) {
-		// Node/CommonJS
-		module.exports = function( root, jQuery ) {
-			if ( jQuery === undefined ) {
-				// require('jQuery') returns a factory that requires window to
-				// build a jQuery instance, we normalize how we use modules
-				// that require this pattern but the window provided is a noop
-				// if it's defined (how jquery works)
-				if ( typeof window !== 'undefined' ) {
-					jQuery = require('jquery');
-				}
-				else {
-					jQuery = require('jquery')(root);
-				}
-			}
-			factory(jQuery);
-			return jQuery;
-		};
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
+    // UMD start
+    // https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
+    if (typeof define === 'function' && define.amd) {
+	// AMD. Register as an anonymous module.
+	define(['jquery'], factory);
+    }
+    else if (typeof module === 'object' && module.exports) {
+	// Node/CommonJS
+	module.exports = function( root, jQuery ) {
+	    if ( jQuery === undefined ) {
+		// require('jQuery') returns a factory that requires window to
+		// build a jQuery instance, we normalize how we use modules
+		// that require this pattern but the window provided is a noop
+		// if it's defined (how jquery works)
+		if ( typeof window !== 'undefined' ) {
+		    jQuery = require('jquery');
+		}
+		else {
+		    jQuery = require('jquery')(root);
+		}
+	    }
+	    
+	    factory(jQuery);
+		
+	    return jQuery;
+	};
+    }
+    else {
+        // Browser globals
+	factory(jQuery);
+    }
 }(function ($) {
-	//IE8 indexOf polyfill
-	var indexOf = [].indexOf || function(item) {
-		for (var i = 0, l = this.length; i < l; i++) {
-			if (i in this && this[i] === item) {
-				return i;
-			}
-		}
-		return -1;
-	};
+    //IE8 indexOf polyfill
+    var indexOf = [].indexOf || function(item) {
+        for (var i = 0, l = this.length; i < l; i++) {
+	    if (i in this && this[i] === item) {
+		return i;
+	    }
+	}
+	    
+	return -1;
+    };
 
-	var pluginName = "notify";
-	var pluginClassName = pluginName + "js";
-	var blankFieldName = pluginName + "!blank";
+    var pluginName = "notify";
+    var pluginClassName = pluginName + "js";
+    var blankFieldName = pluginName + "!blank";
+    var positions = {
+	t: "top",
+	m: "middle",
+	b: "bottom",
+	l: "left",
+	c: "center",
+	r: "right"
+    };
+	
+    var hAligns = ["l", "c", "r"];
+    var vAligns = ["t", "m", "b"];
+    var mainPositions = ["t", "b", "l", "r"];
+    var opposites = {
+	t: "b",
+	m: null,
+	b: "t",
+	l: "r",
+	c: null,
+	r: "l"
+    };
 
-	var positions = {
-		t: "top",
-		m: "middle",
-		b: "bottom",
-		l: "left",
-		c: "center",
-		r: "right"
-	};
-	var hAligns = ["l", "c", "r"];
-	var vAligns = ["t", "m", "b"];
-	var mainPositions = ["t", "b", "l", "r"];
-	var opposites = {
-		t: "b",
-		m: null,
-		b: "t",
-		l: "r",
-		c: null,
-		r: "l"
-	};
+    var parsePosition = function(str) {
+	var pos;
+	pos = [];
+	$.each(str.split(/\W+/), function(i, word) {
+	    var w;
+	    w = word.toLowerCase().charAt(0);
+	    if (positions[w]) {
+		return pos.push(w);
+	    }
+	});
+	    
+	return pos;
+    };
 
-	var parsePosition = function(str) {
-		var pos;
-		pos = [];
-		$.each(str.split(/\W+/), function(i, word) {
-			var w;
-			w = word.toLowerCase().charAt(0);
-			if (positions[w]) {
-				return pos.push(w);
-			}
-		});
-		return pos;
-	};
+    var styles = {};
+    var coreStyle = {
+	name: "core",
+	    html: "<div class=\"" + pluginClassName + "-wrapper\">\n	<div class=\"" + pluginClassName + "-arrow\"></div>\n	<div class=\"" + pluginClassName + "-container\"></div>\n</div>",
+	    css: "." + pluginClassName + "-corner {\n	position: fixed;\n	margin: 5px;\n	z-index: 1050;\n}\n\n." + pluginClassName + "-corner ." + pluginClassName + "-wrapper,\n." + pluginClassName + "-corner ." + pluginClassName + "-container {\n	position: relative;\n	display: block;\n	height: inherit;\n	width: inherit;\n	margin: 3px;\n}\n\n." + pluginClassName + "-wrapper {\n	z-index: 1;\n	position: absolute;\n	display: inline-block;\n	height: 0;\n	width: 0;\n}\n\n." + pluginClassName + "-container {\n	display: none;\n	z-index: 1;\n	position: absolute;\n}\n\n." + pluginClassName + "-hidable {\n	cursor: pointer;\n}\n\n[data-notify-text],[data-notify-html] {\n	position: relative;\n}\n\n." + pluginClassName + "-arrow {\n	position: absolute;\n	z-index: 2;\n	width: 0;\n	height: 0;\n}"
+    };
 
-	var styles = {};
+    var stylePrefixes = { "border-radius": ["-webkit-", "-moz-"] };
+    var getStyle = function(name) {
+	return styles[name];
+    };
 
-	var coreStyle = {
-		name: "core",
-		html: "<div class=\"" + pluginClassName + "-wrapper\">\n	<div class=\"" + pluginClassName + "-arrow\"></div>\n	<div class=\"" + pluginClassName + "-container\"></div>\n</div>",
-		css: "." + pluginClassName + "-corner {\n	position: fixed;\n	margin: 5px;\n	z-index: 1050;\n}\n\n." + pluginClassName + "-corner ." + pluginClassName + "-wrapper,\n." + pluginClassName + "-corner ." + pluginClassName + "-container {\n	position: relative;\n	display: block;\n	height: inherit;\n	width: inherit;\n	margin: 3px;\n}\n\n." + pluginClassName + "-wrapper {\n	z-index: 1;\n	position: absolute;\n	display: inline-block;\n	height: 0;\n	width: 0;\n}\n\n." + pluginClassName + "-container {\n	display: none;\n	z-index: 1;\n	position: absolute;\n}\n\n." + pluginClassName + "-hidable {\n	cursor: pointer;\n}\n\n[data-notify-text],[data-notify-html] {\n	position: relative;\n}\n\n." + pluginClassName + "-arrow {\n	position: absolute;\n	z-index: 2;\n	width: 0;\n	height: 0;\n}"
-	};
+    var removeStyle = function(name) {
+	if (!name) {
+	    throw "Missing Style name";
+	}
+	    
+	if (styles[name]) {
+	    delete styles[name];
+	}
+    };
 
-	var stylePrefixes = {
-		"border-radius": ["-webkit-", "-moz-"]
-	};
-
-	var getStyle = function(name) {
-		return styles[name];
-	};
-
-	var removeStyle = function(name) {
-		if (!name) {
-			throw "Missing Style name";
-		}
-		if (styles[name]) {
-			delete styles[name];
-		}
-	};
-
-	var addStyle = function(name, def) {
-		if (!name) {
-			throw "Missing Style name";
-		}
-		if (!def) {
-			throw "Missing Style definition";
-		}
-		if (!def.html) {
-			throw "Missing Style HTML";
-		}
-		//remove existing style
-		var existing = styles[name];
-		if (existing && existing.cssElem) {
-			if (window.console) {
-				console.warn(pluginName + ": overwriting style '" + name + "'");
-			}
-			styles[name].cssElem.remove();
-		}
-		def.name = name;
-		styles[name] = def;
-		var cssText = "";
-		if (def.classes) {
-			$.each(def.classes, function(className, props) {
-				cssText += "." + pluginClassName + "-" + def.name + "-" + className + " {\n";
-				$.each(props, function(name, val) {
-					if (stylePrefixes[name]) {
-						$.each(stylePrefixes[name], function(i, prefix) {
-							return cssText += "	" + prefix + name + ": " + val + ";\n";
-						});
-					}
-					return cssText += "	" + name + ": " + val + ";\n";
-				});
-				return cssText += "}\n";
+    var addStyle = function(name, def) {
+        if (!name) {
+	    throw "Missing Style name";
+	}
+	    
+	if (!def) {
+	    throw "Missing Style definition";
+	}
+	    
+	if (!def.html) {
+	    throw "Missing Style HTML";
+	}
+	    
+	//remove existing style
+	var existing = styles[name];
+	if (existing && existing.cssElem) {
+	    if (window.console) {
+		console.warn(pluginName + ": overwriting style '" + name + "'");
+	    }
+		
+	    styles[name].cssElem.remove();
+	}
+	    
+	def.name = name;
+	styles[name] = def;
+	var cssText = "";
+	if (def.classes) {
+	    $.each(def.classes, function(className, props) {
+		cssText += "." + pluginClassName + "-" + def.name + "-" + className + " {\n";
+		$.each(props, function(name, val) {
+		    if (stylePrefixes[name]) {
+			$.each(stylePrefixes[name], function(i, prefix) {
+			    return cssText += "	" + prefix + name + ": " + val + ";\n";
 			});
-		}
-		if (def.css) {
-			cssText += "/* styles for " + def.name + " */\n" + def.css;
-		}
-		if (cssText) {
-			def.cssElem = insertCSS(cssText);
-			def.cssElem.attr("id", "notify-" + def.name);
-		}
-		var fields = {};
-		var elem = $(def.html);
-		findFields("html", elem, fields);
-		findFields("text", elem, fields);
-		def.fields = fields;
-	};
-
-	var insertCSS = function(cssText) {
-		var e, elem, error;
-		elem = createElem("style");
-		elem.attr("type", 'text/css');
-		$("head").append(elem);
-		try {
-			elem.html(cssText);
-		} catch (_) {
-			elem[0].styleSheet.cssText = cssText;
-		}
-		return elem;
-	};
-
-	var findFields = function(type, elem, fields) {
-		var attr;
-		if (type !== "html") {
-			type = "text";
-		}
-		attr = "data-notify-" + type;
-		return find(elem, "[" + attr + "]").each(function() {
-			var name;
-			name = $(this).attr(attr);
-			if (!name) {
-				name = blankFieldName;
-			}
-			fields[name] = type;
+		    }
+			
+		    return cssText += "	" + name + ": " + val + ";\n";
 		});
-	};
+		    
+		return cssText += "}\n";
+	    });
+	}
+	    
+	if (def.css) {
+	    cssText += "/* styles for " + def.name + " */\n" + def.css;
+	}
+	    
+	if (cssText) {
+	    def.cssElem = insertCSS(cssText);
+	    def.cssElem.attr("id", "notify-" + def.name);
+	}
+	    
+	var fields = {};
+	var elem = $(def.html);
+	findFields("html", elem, fields);
+	findFields("text", elem, fields);
+	def.fields = fields;
+    };
 
-	var find = function(elem, selector) {
-		if (elem.is(selector)) {
-			return elem;
-		} else {
-			return elem.find(selector);
-		}
-	};
+    var insertCSS = function(cssText) {
+	var e, elem, error;
+	elem = createElem("style");
+	elem.attr("type", 'text/css');
+	$("head").append(elem);
+	try {
+	    elem.html(cssText);
+	}
+	catch (_) {
+	    elem[0].styleSheet.cssText = cssText;
+	}
+	    
+	return elem;
+    };
 
-	var pluginOptions = {
-		clickToHide: true,
-		autoHide: true,
-		autoHideDelay: 5000,
-		arrowShow: true,
-		arrowSize: 5,
-		breakNewLines: true,
-		elementPosition: "bottom",
-		globalPosition: "top right",
-		style: "bootstrap",
-		className: "error",
-		showAnimation: "slideDown",
-		showDuration: 400,
-		hideAnimation: "slideUp",
-		hideDuration: 200,
-		gap: 5
-	};
+    var findFields = function(type, elem, fields) {
+	var attr;
+	if (type !== "html") {
+	    type = "text";
+	}
+	    
+	attr = "data-notify-" + type;
+	    
+	return find(elem, "[" + attr + "]").each(function() {
+	    var name;
+	    name = $(this).attr(attr);
+	    if (!name) {
+		name = blankFieldName;
+	    }
+		
+	    fields[name] = type;
+	});
+    };
 
-	var inherit = function(a, b) {
-		var F;
-		F = function() {};
-		F.prototype = a;
-		return $.extend(true, new F(), b);
-	};
+    var find = function(elem, selector) {
+	if (elem.is(selector)) {
+	    return elem;
+	}
+	else {
+	    return elem.find(selector);
+	}
+    };
 
-	var defaults = function(opts) {
-		return $.extend(pluginOptions, opts);
-	};
+    var pluginOptions = {
+	clickToHide: true,
+	autoHide: true,
+	autoHideDelay: 5000,
+	arrowShow: true,
+	arrowSize: 5,
+	breakNewLines: true,
+	elementPosition: "bottom",
+	globalPosition: "top right",
+	style: "bootstrap",
+	className: "error",
+	showAnimation: "slideDown",
+	showDuration: 400,
+	hideAnimation: "slideUp",
+	hideDuration: 200,
+	gap: 5
+    };
 
-	var createElem = function(tag) {
-		return $("<" + tag + "></" + tag + ">");
-	};
+    var inherit = function(a, b) {
+	var F;
+	F = function() {};
+	    F.prototype = a;
+	    
+	return $.extend(true, new F(), b);
+    };
 
-	var globalAnchors = {};
+    var defaults = function(opts) {
+	return $.extend(pluginOptions, opts);
+    };
 
-	var getAnchorElement = function(element) {
-		var radios;
-		if (element.is('[type=radio]')) {
-			radios = element.parents('form:first').find('[type=radio]').filter(function(i, e) {
-				return $(e).attr("name") === element.attr("name");
-			});
-			element = radios.first();
-		}
-		return element;
-	};
+    var createElem = function(tag) {
+	return $("<" + tag + "></" + tag + ">");
+    };
 
-	var incr = function(obj, pos, val) {
-		var opp, temp;
-		if (typeof val === "string") {
-			val = parseInt(val, 10);
-		} else if (typeof val !== "number") {
-			return;
-		}
-		if (isNaN(val)) {
-			return;
-		}
-		opp = positions[opposites[pos.charAt(0)]];
+    var globalAnchors = {};
+
+    var getAnchorElement = function(element) {
+	var radios;
+	if (element.is('[type=radio]')) {
+	    radios = element.parents('form:first').find('[type=radio]').filter(function(i, e) {
+		return $(e).attr("name") === element.attr("name");
+	    });
+		
+	    element = radios.first();
+	}
+	    
+	return element;
+    };
+
+    var incr = function(obj, pos, val) {
+	var opp, temp;
+	if (typeof val === "string") {
+	    val = parseInt(val, 10);
+	}
+	else if (typeof val !== "number") {
+	    return;
+	}
+	    
+	if (isNaN(val)) {
+	    return;
+	}
+	    
+	opp = positions[opposites[pos.charAt(0)]];
 		temp = pos;
 		if (obj[opp] !== undefined) {
 			pos = positions[opp.charAt(0)];
