e9a59d16f6a449df0ebc98a173e389f0b48590e6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-07-06 22:40:26 +0300

    Update app.js

--- JSCoreAdvanced/07WorkshopLandingPage/scripts/app.js
+++ JSCoreAdvanced/07WorkshopLandingPage/scripts/app.js
@@ -12,36 +12,18 @@
         if (validUsername && validLecturerName && filledInputFields) {
             $.notify("Consultation added", "success");
             addNewConsultation();
-        } else {
+        }
+        else {
             $.notify("Try again", "error");
         }
     }
 
-    function addNewConsultation() {
-        let lecturerName = $('#lecturer option:selected').val();
-        let dateAndTime = $('#datetimepicker').val().split(' ');
-        let yearMonthDay = dateAndTime[0].split('/');
-        let monthDay = `${yearMonthDay[1]}/${yearMonthDay[2]}`;
-
-        let time = dateAndTime[1];
-
-        $('.education article:nth-child(3) .box-body ul')
-            .append(`<li><span>${lecturerName} - ${monthDay} - ${time}</span><i class="fas fa-chevron-circle-right"></i></li>`);
-
-        increaseConsultationsCount();
-    }
-
-    function increaseConsultationsCount() {
-        let consultationsCount = $('.education article:nth-child(3) .box-footer span');
-        let currentCount = consultationsCount.text();
-        consultationsCount.text(++currentCount);
-    }
-
-    function filledInputFieldsValidation() {
+    function usernameValidation() {
         let isValid = false;
+        const regex = /^([A-Za-z])([A-Za-z0-9_]{2,24})$/gm;
 
-        let dateTimeInputField = $('#datetimepicker').val();
-        if (dateTimeInputField !== '') {
+        let username = $('#username').val();
+        if (regex.test(username)) {
             isValid = true;
         }
 
@@ -59,15 +41,33 @@
         return isValid;
     }
 
-    function usernameValidation() {
+    function filledInputFieldsValidation() {
         let isValid = false;
-        const regex = /^([A-Za-z])([A-Za-z0-9_]{2,24})$/gm;
 
-        let username = $('#username').val();
-        if (regex.test(username)) {
+        let dateTimeInputField = $('#datetimepicker').val();
+        if (dateTimeInputField !== '') {
             isValid = true;
         }
 
         return isValid;
+    }
+
+    function addNewConsultation() {
+        let lecturerName = $('#lecturer option:selected').val();
+        let dateAndTime = $('#datetimepicker').val().split(' ');
+        let yearMonthDay = dateAndTime[0].split('/');
+        let monthDay = `${yearMonthDay[1]}/${yearMonthDay[2]}`;
+
+        let time = dateAndTime[1];
+
+        $('.education article:nth-child(3) .box-body ul').append(`<li><span>${lecturerName} - ${monthDay} - ${time}</span><i class="fas fa-chevron-circle-right"></i></li>`);
+
+        increaseConsultationsCount();
+    }
+
+    function increaseConsultationsCount() {
+        let consultationsCount = $('.education article:nth-child(3) .box-footer span');
+        let currentCount = consultationsCount.text();
+        consultationsCount.text(++currentCount);
     }
 }());
