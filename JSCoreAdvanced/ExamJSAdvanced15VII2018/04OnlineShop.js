b057094bc7f47f391f5e59dbc3b6920fff90b5b5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-07-09 22:45:50 +0300

    Update 04OnlineShop.js

--- JSCoreAdvanced/ExamJSAdvanced15VII2018/04OnlineShop.js
+++ JSCoreAdvanced/ExamJSAdvanced15VII2018/04OnlineShop.js
@@ -7,11 +7,11 @@ function onlineShop(selector) {
         <input class="input1" id="price" type="number" min="1" max="999999" value="1"><label class="text">BGN</label>
         <input class="input1" id="quantity" type="number" min="1" value="1"><label class="text">Qty.</label>
         <button id="submit" class="button" disabled>Submit</button>
-        <br><br>
+        <br>
+        <br>
         <label class="field">Inventory:</label>
         <br>
-        <ul class="display">
-        </ul>
+        <ul class="display"></ul>
         <br>
         <label class="field">Capacity:</label><input id="capacity" readonly>
         <label class="field">(maximum capacity is 150 items.)</label>
@@ -19,6 +19,7 @@ function onlineShop(selector) {
         <label class="field">Price:</label><input id="sum" readonly>
         <label class="field">BGN</label>
     </div>`;
+    
     $(selector).html(form);
     const maxCapacity = 150;
 
@@ -27,7 +28,6 @@ function onlineShop(selector) {
     let $inputPrice = $('#price');
     let $inputQuantity = $('#quantity');
     let $buttonSubmit = $('#submit');
-
     let $ulInventory = $('ul[class="display"]');
     //let $ulInventory = $('.display');
     let $outputCapacity = $('#capacity');
@@ -65,7 +65,7 @@ function onlineShop(selector) {
             totalPrice += price;
             $outputPrice.val(totalPrice);            
         }
-        else{            
+        else {            
             $outputCapacity.val('full');
             $outputCapacity.addClass('fullCapacity');
             disableInputFields();
