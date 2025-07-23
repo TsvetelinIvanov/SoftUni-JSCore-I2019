0b8b9628cbe4a4a5cbc716189edd24751c0a269b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-07-24 00:06:15 +0300

    Update 02Warehouse.js

--- JSCoreAdvanced/ExamRetakeJSAdvanced19XII2018/02Warehouse.js
+++ JSCoreAdvanced/ExamRetakeJSAdvanced19XII2018/02Warehouse.js
@@ -1,14 +1,13 @@
 class Warehouse {
-
     get capacity() {
         return this._capacity;
     }
 
     set capacity(givenSpace) {
-
         if (typeof givenSpace === 'number' && givenSpace > 0) {
             return this._capacity = givenSpace;
-        } else {
+        }
+        else {
             throw `Invalid given warehouse space`;
         }
     }
@@ -19,20 +18,17 @@ class Warehouse {
     }
 
     addProduct(type, product, quantity) {
-
         let addedQuantity = ((this.capacity - this.occupiedCapacity()) - quantity);
         let output;
-
         if (addedQuantity >= 0) {
-
             if (this.availableProducts[type].hasOwnProperty(product) === false) {
                 this.availableProducts[type][product] = 0;
             }
 
             this.availableProducts[type][product] += quantity;
             output = this.availableProducts[type];
-
-        } else {
+        }
+        else {
             throw `There is not enough space or the warehouse is already full`;
         }
 
@@ -40,15 +36,10 @@ class Warehouse {
     }
 
     orderProducts(type) {
-
         let output;
-        let sortedKeys = Object.keys(this.availableProducts[type])
-            .sort((a, b) => this.availableProducts[type][b] - this.availableProducts[type][a]);
-
+        let sortedKeys = Object.keys(this.availableProducts[type]).sort((a, b) => this.availableProducts[type][b] - this.availableProducts[type][a]);
         let newObj = {};
-
         for (let product of sortedKeys) {
-
             if (newObj.hasOwnProperty(product) === false) {
                 newObj[product] = 0;
             }
@@ -63,19 +54,12 @@ class Warehouse {
     }
 
     occupiedCapacity() {
-
         let output = 0;
-        let productsCount = Object.keys(this.availableProducts['Food']).length +
-            Object.keys(this.availableProducts['Drink']).length;
-
+        let productsCount = Object.keys(this.availableProducts['Food']).length + Object.keys(this.availableProducts['Drink']).length;
         if (productsCount > 0) {
-
             let quantityInStock = 0;
-
             for (let type of Object.keys(this.availableProducts)) {
-
                 for (let product of Object.keys(this.availableProducts[type])) {
-
                     quantityInStock += this.availableProducts[type][product];
                 }
             }
@@ -87,18 +71,16 @@ class Warehouse {
     }
 
     revision() {
-
         let output = "";
-
         if (this.occupiedCapacity() > 0) {
-
             for (let type of Object.keys(this.availableProducts)) {
                 output += `Product type - [${type}]\n`;
                 for (let product of Object.keys(this.availableProducts[type])) {
                     output += `- ${product} ${this.availableProducts[type][product]}\n`;
                 }
             }
-        } else {
+        }
+        else {
             output = 'The warehouse is empty';
         }
 
@@ -106,21 +88,19 @@ class Warehouse {
     }
 
     scrapeAProduct(product, quantity) {
-
-        let type = Object.keys(this.availableProducts).find(t =>          Object.keys(this.availableProducts[t]).includes(product));
+        let type = Object.keys(this.availableProducts).find(t => Object.keys(this.availableProducts[t]).includes(product));
         let output;
-
         if (type !== undefined) {
-
             if (quantity <= this.availableProducts[type][product]) {
                 this.availableProducts[type][product] -= quantity;
-            } else {
+            }
+            else {
                 this.availableProducts[type][product] = 0;
             }
 
             output = this.availableProducts[type];
-
-        } else {
+        }
+        else {
             throw `${product} do not exists`;
         }
 
