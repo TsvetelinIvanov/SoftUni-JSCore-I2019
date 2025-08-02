995f0d84c34f792ad83c9c3c579bf9726a8c90a3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-08-02 11:22:59 +0300

    Update 03CreateBooksRESTService.js

--- JSCoreApplications/03AsynchronousProgrammingExercises/03CreateBooksRESTService.js
+++ JSCoreApplications/03AsynchronousProgrammingExercises/03CreateBooksRESTService.js
@@ -1,4 +1,4 @@
-function serviceBooks(){
+function serviceBooks() {
     //const baseUrl = "https://baas.kinvey.com/appdata/kid_rkOE5IY5f/books";
     const baseUrl = "https://baas.kinvey.com/appdata/kid_H1y9kL-_N/books";
     const auth = `Basic ${btoa("guest:guest")}`;
@@ -7,23 +7,24 @@ function serviceBooks(){
     $("#add").click(addBook);
     $("#load").click(loadBooks);
 
-    async function loadBooks(){
+    async function loadBooks() {
         let $books = $("#books");
         $books.empty();
         let books = await $.ajax({
             url: baseUrl,
             method: "GET",
             headers
-        });        
-        for (let book of books){
+        });
+        
+        for (let book of books) {
             $books.append(createBook(book));
         }        
     }
 
-    async function addBook(){
+    async function addBook() {
         let [title, author, isbn] = $("#newBooksFiles input").map((i, item) => $(item).val());
         
-        if(title && author && isbn){          
+        if (title && author && isbn) {          
             let bookData = {title, author, isbn};
             //$("#newBooksFiles > input").val("");
             $("#newBooksFiles input").val("");            
@@ -32,13 +33,14 @@ function serviceBooks(){
                 url: baseUrl,
                 headers,
                 data: JSON.stringify(bookData)
-            });            
+            });
+            
             let $liBook = createBook(book);
             $("#books").append($liBook);
         }
     }
 
-    function createBook(book){
+    function createBook(book) {
         let $li = $("<li>");
         let $span = $(`<span>Title: ${book.title}; Author: ${book.author}; ISBN: ${book.isbn};</span>`);
         let $a = $('<a href="#">[Delete]</a>').click(() => {
@@ -49,8 +51,10 @@ function serviceBooks(){
             })
             .then(() => $li.remove());
         });
+        
         $span.append($a);
         $li.append($span);
+        
         return $li;
     }
 }
