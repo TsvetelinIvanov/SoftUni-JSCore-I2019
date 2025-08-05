a412bf806f2e275e3ef2c8370d219262123efd20
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-08-05 06:36:34 +0300

    Update 07SecretKnock.js

--- JSCoreApplications/03AsynchronousProgrammingExercises/07SecretKnock.js
+++ JSCoreApplications/03AsynchronousProgrammingExercises/07SecretKnock.js
@@ -1,29 +1,30 @@
-function doSecretKnock(){
+function doSecretKnock() {
     const baseUrl = "https://baas.kinvey.com/appdata/kid_BJXTsSi-e/knock?query=";
     const kinveyUsername = "guest";
     const kinveyPassword = "guest";
     const base64Auth = btoa(kinveyUsername + ":" + kinveyPassword);
     const headers = {"Authorization": "Basic " + base64Auth};
+    
     let $divContent = $("#content");
     let currentMessage = "Knock Knock.";
     let $pCurrent = $("<p>").text(currentMessage);
     $divContent.append($pCurrent);
     getNext(currentMessage);
 
-    function getNext(message){
+    function getNext(message) {
         let request = {
             method: "GET",
             url: baseUrl + message,
             headers
         };
 
-        $.ajax(request).then(function(object){
-            if (object.answer){
+        $.ajax(request).then(function(object) {
+            if (object.answer) {
                 let $pNext = $("<p>").text(object.answer);
                 $divContent.append($pNext);               
             }
 
-            if (object.message){
+            if (object.message) {
                 let $pNext = $("<p>").text(object.message);
                 $divContent.append($pNext);
                 currentMessage = object.message;
