9eb44a13aeea64bf6b071d9482747ab3a972ce37
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-08-06 11:28:59 +0300

    Update 02Blog.js

--- JSCoreApplications/03AsynchronousProgrammingLab/02Blog.js
+++ JSCoreApplications/03AsynchronousProgrammingLab/02Blog.js
@@ -1,4 +1,4 @@
-function attachEvents(){
+function attachEvents() {
     //const kinveyAppId = "kid_S1htVfcmm";
     //const kinveyAppId = "kid_B1KyOfAZe";
     const kinveyAppId = "kid_H1y9kL-_N";
@@ -12,39 +12,39 @@ function attachEvents(){
     $('#btnLoadPosts').click(loadPostsClick);
     $('#btnViewPost').click(viewPostClick);
     
-    function loadPostsClick(){
+    function loadPostsClick() {
         let loadPostRequest = {
             url: serviceUrl + "/posts",
             headers: authHeaders
         };
 
         $.ajax(loadPostRequest)
-         .then(displayPosts)
-         .catch(displayError);
+            .then(displayPosts)
+            .catch(displayError);
     }
 
-    function displayPosts(posts){
+    function displayPosts(posts) {
         let $posts = $('#posts');
         $posts.empty();
-        for (let post of posts){
+        for (let post of posts) {
             let $optionPost = $('<option>').val(post._id).text(post.title);
             $posts.append($optionPost);
         }
     }
 
-    function displayError(error){
+    function displayError(error) {
         let $divError = $('<div>').text('Error: ' + error.status + ' (' + error.statusText + ')');
         $(document.body).prepend($divError);
-        setTimeout(function(){
-            $($divError).fadeOut(function(){
+        setTimeout(function() {
+            $($divError).fadeOut(function() {
                 $($divError).remove;
             });
         }, 3000);
     }
 
-    function viewPostClick(){
+    function viewPostClick() {
         let selectedPostId = $('#posts').val();
-        if (!selectedPostId){
+        if (!selectedPostId) {
             return;
         }
 
@@ -60,11 +60,11 @@ function attachEvents(){
         });
 
         Promise.all([requestPosts, requestComments])
-         .then(displayPostWithComments)
-         .catch(displayError);
+            .then(displayPostWithComments)
+            .catch(displayError);
     }
 
-    function displayPostWithComments([post, comments]){
+    function displayPostWithComments([post, comments]) {
         let $h1postTitle = $('#post-title');
         let $ulPostBody = $('#post-body');
         let $ulPostComments = $('#post-comments');
@@ -72,7 +72,7 @@ function attachEvents(){
         $h1postTitle.text(post.title);
         $ulPostBody.text(post.body);
         $ulPostComments.empty();
-        for (let comment of comments){
+        for (let comment of comments) {
             let $li = $('<li>').text(comment.text);
             $ulPostComments.append($li);
         }
