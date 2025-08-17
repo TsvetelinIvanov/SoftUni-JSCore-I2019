79d37e376ebb8bc16f11aa7b13fc15a3345842dc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-08-17 05:46:39 +0300

    Update 01ListTowns.js

--- JSCoreApplications/05TemplatingExercises/01ListTowns.js
+++ JSCoreApplications/05TemplatingExercises/01ListTowns.js
@@ -1,12 +1,13 @@
-function attachEvents(){
+function attachEvents() {
     $('#btnLoadTowns').click(async () => {
         let $towns = $('#towns');
         let towns = $towns.val().split(',').filter(t => t !== '').map(t => {
             let object = {town: t};
+            
             return object;
         });
 
-        if (towns){
+        if (towns) {
             let source = $('#towns-template').html();
             let template = Handlebars.compile(source);
             let context = {towns};
