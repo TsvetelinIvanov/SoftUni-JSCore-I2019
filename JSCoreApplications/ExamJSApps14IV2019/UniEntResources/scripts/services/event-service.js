2d461fb53590038871198cc8f4148221c79fa7af
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-09-01 20:24:05 +0300

    Update event-service.js

--- JSCoreApplications/ExamJSApps14IV2019/UniEntResources/scripts/services/event-service.js
+++ JSCoreApplications/ExamJSApps14IV2019/UniEntResources/scripts/services/event-service.js
@@ -3,24 +3,24 @@ const eventService = (() => {
         return kinvey.post('appdata', 'events', 'kinvey', data);
     }
 
-    function showEvents(){
+    function showEvents() {
         return kinvey.get('appdata', `events?query={}&sort={"peopleInterestedIn": -1}`, 'kinvey');
     }
 
-    function showEvent(id){
+    function showEvent(id) {
         return kinvey.get('appdata', `events/${id}`, 'kinvey');
     }
 
-    function showMyEvents(userId){
+    function showMyEvents(userId) {
         return kinvey.get('appdata', `events/?query={"organizer":"${userId}"}`, 'kinvey');
         //return kinvey.get('appdata', `events/?query={"_acl.creator":"${userId}"}`, 'kinvey');
     }
 
-    function deleteEvent(id){
+    function deleteEvent(id) {
         return kinvey.remove('appdata', `events/${id}`, 'kinvey');
     }
 
-    function editEvent(id, data){
+    function editEvent(id, data) {
         return kinvey.update('appdata', `events/${id}`, 'kinvey', data);
     }
 
