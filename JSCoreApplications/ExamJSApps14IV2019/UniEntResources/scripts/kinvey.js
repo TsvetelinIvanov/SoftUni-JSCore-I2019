4e58cc723625047370f2e3ea1aa98f3e9cff512f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-09-03 20:55:20 +0300

    Update kinvey.js

--- JSCoreApplications/ExamJSApps14IV2019/UniEntResources/scripts/kinvey.js
+++ JSCoreApplications/ExamJSApps14IV2019/UniEntResources/scripts/kinvey.js
@@ -1,52 +1,55 @@
 const kinvey = (() => {
-  const BASE_URL = 'https://baas.kinvey.com/';
-  const APP_KEY = 'kid_H1y9kL-_N'; // APP KEY HERE
-  const APP_SECRET = '270909213f424f249f5d6da36f8c31f3'; // APP SECRET HERE
+    const BASE_URL = 'https://baas.kinvey.com/';
+    const APP_KEY = 'kid_H1y9kL-_N'; // APP KEY HERE
+    const APP_SECRET = '270909213f424f249f5d6da36f8c31f3'; // APP SECRET HERE
 
-  function makeAuth(auth) {
-    if (auth == 'basic') {
-      return {
-        'Authorization': `Basic ${btoa(APP_KEY + ':' + APP_SECRET)}`
-      }
-    } else {
-      return {
-        'Authorization': `Kinvey ${sessionStorage.getItem('authtoken')}`
-      }
+    function makeAuth(auth) {
+        if (auth == 'basic') {
+            return {
+                'Authorization': `Basic ${btoa(APP_KEY + ':' + APP_SECRET)}`
+            }
+        }
+        else {
+            return {
+                'Authorization': `Kinvey ${sessionStorage.getItem('authtoken')}`
+            }
+        }
     }
-  }
 
-  function makeRequest(method, collection, endpoint, auth) {
+    function makeRequest(method, collection, endpoint, auth) {
+        return {
+            url: BASE_URL + collection + '/' + APP_KEY + '/' + endpoint,
+            method,
+            headers: makeAuth(auth)
+        }
+    }
+
+    function get(collection, endpoint, auth) {
+        return $.ajax(makeRequest('GET', collection, endpoint, auth));
+    }
+
+    function post(collection, endpoint, auth, data) {
+        let req = makeRequest('POST', collection, endpoint, auth);
+        req.data = data;
+      
+        return $.ajax(req);
+    }  
+
+    function update(collection, endpoint, auth, data) {
+        let req = makeRequest('PUT', collection, endpoint, auth);
+        req.data = data;
+      
+        return $.ajax(req);
+    }
+
+    function remove(collection, endpoint, auth) {
+        return $.ajax(makeRequest('DELETE', collection, endpoint, auth));
+    }
+
     return {
-      url: BASE_URL + collection + '/' + APP_KEY + '/' + endpoint,
-      method,
-      headers: makeAuth(auth)
+        get,
+        post,
+        update,
+        remove
     }
-  }
-
-  function get(collection, endpoint, auth) {
-    return $.ajax(makeRequest('GET', collection, endpoint, auth));
-  }
-
-  function post(collection, endpoint, auth, data) {
-    let req = makeRequest('POST', collection, endpoint, auth);
-    req.data = data;
-    return $.ajax(req);
-  }  
-
-  function update(collection, endpoint, auth, data) {
-    let req = makeRequest('PUT', collection, endpoint, auth);
-    req.data = data;
-    return $.ajax(req);
-  }
-
-  function remove(collection, endpoint, auth) {
-    return $.ajax(makeRequest('DELETE', collection, endpoint, auth));
-  }
-
-  return {
-    get,
-    post,
-    update,
-    remove
-  }
 })()
