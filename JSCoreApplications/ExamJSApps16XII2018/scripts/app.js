9c37f41f9af8550cdcf71acfd877ac26ef47da5f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-09-12 09:38:12 +0300

    Update app.js

--- JSCoreApplications/ExamJSApps16XII2018/scripts/app.js
+++ JSCoreApplications/ExamJSApps16XII2018/scripts/app.js
@@ -1,31 +1,30 @@
 const handlers = {}
 
 $(() => {
-  const app = Sammy('#container', function () {
-    this.use('Handlebars', 'hbs');
+    const app = Sammy('#container', function() {
+        this.use('Handlebars', 'hbs');
 
-    // home page routes
-    this.get('/index.html', handlers.getHome);
-    this.get('/', handlers.getHome);
-    this.get('#/home', handlers.getHome);
+        // home page routes
+        this.get('/index.html', handlers.getHome);
+        this.get('/', handlers.getHome);
+        this.get('#/home', handlers.getHome);
 
-    // user routes
-    this.get('#/register', handlers.getRegister);
-    this.get('#/login', handlers.getLogin);
-    this.post('#/register', handlers.registerUser);
-    this.post('#/login', handlers.loginUser);
-    this.get('#/logout', handlers.logoutUser);
+        // user routes
+        this.get('#/register', handlers.getRegister);
+        this.get('#/login', handlers.getLogin);
+        this.post('#/register', handlers.registerUser);
+        this.post('#/login', handlers.loginUser);
+        this.get('#/logout', handlers.logoutUser);
 
-    //pet routes    
-    this.get('#/dashboard(/:category)?', handlers.getDashboard);
-    this.get('#/like/:id', handlers.likePet);
-    this.get('#/createPet', handlers.getCreatePet);
-    this.post('#/createPet', handlers.createPet);
-    this.get('#/myPets', handlers.getMyPets);    
-    this.get('#/delete/:id', handlers.deletePet);
-    this.get('#/details/:id', handlers.getPetDetails);
-    this.post('#/details/:id', handlers.editPetInfo);
-    
-  });
-  app.run('#/home');
+        //pet routes    
+        this.get('#/dashboard(/:category)?', handlers.getDashboard);
+        this.get('#/like/:id', handlers.likePet);
+        this.get('#/createPet', handlers.getCreatePet);
+        this.post('#/createPet', handlers.createPet);
+        this.get('#/myPets', handlers.getMyPets);    
+        this.get('#/delete/:id', handlers.deletePet);
+        this.get('#/details/:id', handlers.getPetDetails);
+        this.post('#/details/:id', handlers.editPetInfo);    
+    });
+    app.run('#/home');
 });
