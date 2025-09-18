f318e36539b106880ce1ad753e2afd6365d71dd7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-09-18 20:18:28 +0300

    Update user-handler.js

--- JSCoreApplications/ExamRetakeJSApps21XII2018/SpotifyResources/scripts/handlers/user-handler.js
+++ JSCoreApplications/ExamRetakeJSApps21XII2018/SpotifyResources/scripts/handlers/user-handler.js
@@ -1,35 +1,37 @@
-handlers.getRegister = function (context) {
+handlers.getRegister = function(context) {
     context.loadPartials({
         header: './templates/common/header.hbs',
         footer: './templates/common/footer.hbs'
-    }).then(function () {
+    }).then(function() {
         this.partial('./templates/register.hbs');
-    }).catch(function (error) {
+    }).catch(function(error) {
         notify.handleError(error);
     });
 }
 
-handlers.getLogin = function (context) {
+handlers.getLogin = function(context) {
     context.loadPartials({
         header: './templates/common/header.hbs',
         footer: './templates/common/footer.hbs'
-    }).then(function () {
+    }).then(function() {
         this.partial('./templates/login.hbs');
     }).catch(function (error) {
         notify.handleError(error);
     });
 }
 
-handlers.registerUser = function (context) {
+handlers.registerUser = function(context) {
     let username = context.params.username;
     let password = context.params.password;
     if (username.length < 3) {
         notify.showError('Username should be at least 3 character long.');
+        
         return;
     }
 
     if (password.length < 6) {
         notify.showError('Password should be at least 6 character long.');
+        
         return;
     }
 
@@ -37,24 +39,24 @@ handlers.registerUser = function (context) {
         userService.saveSession(response);
         notify.showInfo('User registration successful.');
         context.redirect('#/home');
-    }).catch(function (error) {
+    }).catch(function(error) {
         notify.handleError(error);
     });
 }
 
-handlers.loginUser = function (context) {
+handlers.loginUser = function(context) {
     let username = context.params.username;
     let password = context.params.password;
     userService.login(username, password).then((response) => {
         userService.saveSession(response);
         notify.showInfo('Login successful.');
         context.redirect('#/home');
-    }).catch(function (error) {
+    }).catch(function(error) {
         notify.handleError(error);
     });
 }
 
-handlers.logoutUser = function (context) {
+handlers.logoutUser = function(context) {
     userService.logout().then(() => {
         sessionStorage.clear();
         notify.showInfo('Logout successful.');
