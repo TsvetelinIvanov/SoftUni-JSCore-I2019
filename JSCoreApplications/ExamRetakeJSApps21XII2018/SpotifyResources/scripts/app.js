9a20f371cc3bc0d75cb4d33b89a972ec12147cbf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-09-20 01:43:20 +0300

    Update app.js

--- JSCoreApplications/ExamRetakeJSApps21XII2018/SpotifyResources/scripts/app.js
+++ JSCoreApplications/ExamRetakeJSApps21XII2018/SpotifyResources/scripts/app.js
@@ -1,29 +1,30 @@
 const handlers = {}
 
 $(() => {
-  const app = Sammy('#container', function () {
-    this.use('Handlebars', 'hbs');
-    // home page routes
-    this.get('/index.html', handlers.getHome);
-    this.get('/', handlers.getHome);
-    this.get('#/home', handlers.getHome);
+    const app = Sammy('#container', function() {
+        this.use('Handlebars', 'hbs');
+      
+        // home page routes
+        this.get('/index.html', handlers.getHome);
+        this.get('/', handlers.getHome);
+        this.get('#/home', handlers.getHome);
 
-    // user routes
-    this.get('#/register', handlers.getRegister);
-    this.get('#/login', handlers.getLogin);
+        // user routes
+        this.get('#/register', handlers.getRegister);
+        this.get('#/login', handlers.getLogin);
+        this.post('#/register', handlers.registerUser);
+        this.post('#/login', handlers.loginUser);
+        this.get('#/logout', handlers.logoutUser);
 
-    this.post('#/register', handlers.registerUser);
-    this.post('#/login', handlers.loginUser);
-    this.get('#/logout', handlers.logoutUser);
-
-    // songs
-    this.get('#/allSongs', handlers.getAllSongs);
-    this.get('#/createSong', handlers.getCreateSong);
-    this.post('#/createSong', handlers.createSong);
-    this.get('#/like/:id', handlers.likeSong);
-    this.get('#/remove/:id', handlers.removeSong);    
-    this.get('#/listen/:id', handlers.listenSong);
-    this.get('#/mySongs', handlers.getMySongs);
-  });
-  app.run('#/home');
+        // songs
+        this.get('#/allSongs', handlers.getAllSongs);
+        this.get('#/createSong', handlers.getCreateSong);
+        this.post('#/createSong', handlers.createSong);
+        this.get('#/like/:id', handlers.likeSong);
+        this.get('#/remove/:id', handlers.removeSong);    
+        this.get('#/listen/:id', handlers.listenSong);
+        this.get('#/mySongs', handlers.getMySongs);
+    });
+  
+    app.run('#/home');
 });
