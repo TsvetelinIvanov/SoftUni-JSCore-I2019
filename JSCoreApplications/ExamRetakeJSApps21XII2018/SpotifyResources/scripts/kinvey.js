6cdd71ed2cec53f5201713c7d3088bf56622aebb
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-09-20 01:56:59 +0300

    Update kinvey.js

--- JSCoreApplications/ExamRetakeJSApps21XII2018/SpotifyResources/scripts/kinvey.js
+++ JSCoreApplications/ExamRetakeJSApps21XII2018/SpotifyResources/scripts/kinvey.js
@@ -4,52 +4,57 @@ const kinvey = (() => {
     const APP_SECRET = '270909213f424f249f5d6da36f8c31f3'; 
   
     function makeAuth(auth) {
-      if (auth == 'basic') {
-        return {
-          'Authorization': `Basic ${btoa(APP_KEY + ':' + APP_SECRET)}`
+        if (auth == 'basic') {
+            return {
+                'Authorization': `Basic ${btoa(APP_KEY + ':' + APP_SECRET)}`
+            }
         }
-      } else {
-        return {
-          'Authorization': `Kinvey ${sessionStorage.getItem('authtoken')}`
+        else {
+            return {
+                'Authorization': `Kinvey ${sessionStorage.getItem('authtoken')}`
+            }
         }
-      }
     }
   
     function makeRequest(method, collection, endpoint, auth) {
-      return {
-        url: BASE_URL + collection + '/' + APP_KEY + '/' + endpoint,
-        method,
-        headers: makeAuth(auth)
-      }
-    }
-  
-    function post(collection, endpoint, auth, data) {
-      let req = makeRequest('POST', collection, endpoint, auth);
-      req.data = data;
-      return $.ajax(req);
+        return {
+            url: BASE_URL + collection + '/' + APP_KEY + '/' + endpoint,
+            method,
+            headers: makeAuth(auth)
+        }
     }
   
     function get(collection, endpoint, auth, query) {
         if (query) {
             endpoint += query;
         }
-      return $.ajax(makeRequest('GET', collection, endpoint, auth));
+        
+        return $.ajax(makeRequest('GET', collection, endpoint, auth));
+    }
+
+    function post(collection, endpoint, auth, data) {
+        let request = makeRequest('POST', collection, endpoint, auth);
+        request.data = data;
+        
+        return $.ajax(request);
     }
   
     function update(collection, endpoint, auth, data) {
-      let req = makeRequest('PUT', collection, endpoint, auth);
-      req.data = data;
-      return $.ajax(req);
+        let request = makeRequest('PUT', collection, endpoint, auth);
+        request.data = data;
+        
+        return $.ajax(request);
     }
   
     function remove(collection, endpoint, auth) {
-      return $.ajax(makeRequest('DELETE', collection, endpoint, auth));
+        return $.ajax(makeRequest('DELETE', collection, endpoint, auth));
     }
   
     return {
-      get,
-      post,
-      update,
-      remove
+        get,
+        post,
+        update,
+        remove
     }
-  })();
+
+})();
