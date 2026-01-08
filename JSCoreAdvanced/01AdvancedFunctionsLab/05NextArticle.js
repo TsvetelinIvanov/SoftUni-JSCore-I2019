08ad78019589aa5d54e8bc3531ea01cd44961f4b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-01-08 23:00:25 +0200

    Update 05NextArticle.js

--- JSCoreAdvanced/01AdvancedFunctionsLab/05NextArticle.js
+++ JSCoreAdvanced/01AdvancedFunctionsLab/05NextArticle.js
@@ -10,11 +10,12 @@ function getArticleGenerator(articles) {
     }    
 }
 
-getArticleGenerator([
-    "Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
-    "A group of cats is called a clowder.",
-    "Cats have over 20 muscles that control their ears.",
-    "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs.",
-    "The world's largest cat measured 48.5 inches long."
-  ]
+getArticleGenerator(
+    [
+        "Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
+        "A group of cats is called a clowder.",
+        "Cats have over 20 muscles that control their ears.",
+        "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs.",
+        "The world's largest cat measured 48.5 inches long."
+    ]
 )
