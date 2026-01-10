3098f66faca8e73cbffc512dbaed7ecff64f6871
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-01-10 16:22:18 +0200

    Update 03LinkButtons.js

--- JSCoreAdvanced/05JQueryLab/03LinkButtons.js
+++ JSCoreAdvanced/05JQueryLab/03LinkButtons.js
@@ -2,7 +2,7 @@ function attachEvents() {
     let $linkButtons = $('a.button');    
     $linkButtons.on('click', mark);
      
-    function mark(){
+    function mark() {
         $('.selected').removeClass('selected');
         $(this).addClass('selected');
     }
