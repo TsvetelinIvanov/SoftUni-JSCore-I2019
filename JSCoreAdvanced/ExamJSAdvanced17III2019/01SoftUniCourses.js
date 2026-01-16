0b09308f584eb8ee11e1c24a173d0ed3df60107d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-01-17 01:02:17 +0200

    Update 01SoftUniCourses.js

--- JSCoreAdvanced/ExamJSAdvanced17III2019/01SoftUniCourses.js
+++ JSCoreAdvanced/ExamJSAdvanced17III2019/01SoftUniCourses.js
@@ -9,84 +9,84 @@ function solve() {
     $buttonSignMeUp.on('click', addCourses);
  
     function addCourses() {            
-       let $ulMyCourses = $('#myCourses div[class="courseBody"] ul');
-       let $pCost = $('#myCourses div[class="courseFoot"] p'); 
+        let $ulMyCourses = $('#myCourses div[class="courseBody"] ul');
+        let $pCost = $('#myCourses div[class="courseFoot"] p'); 
        
-       let isCheckedInputJSFundamentals = $inputJSFundamentals.is(':checked');
-       let isCheckedInputJSAdvanced = $inputJSAdvanced.is(':checked');
-       let isCheckedInputJSApplications = $inputJSApplications.is(':checked');
-       let isCheckedInputJSWeb = $inputJSWeb.is(':checked');
-       let isCheckedinputRadioOnline = $inputRadioOnline.is(':checked');      
+        let isCheckedInputJSFundamentals = $inputJSFundamentals.is(':checked');
+        let isCheckedInputJSAdvanced = $inputJSAdvanced.is(':checked');
+        let isCheckedInputJSApplications = $inputJSApplications.is(':checked');
+        let isCheckedInputJSWeb = $inputJSWeb.is(':checked');
+        let isCheckedinputRadioOnline = $inputRadioOnline.is(':checked');      
  
-       // let isCheckedInputJSFundamentals;
-       // let isCheckedInputJSAdvanced;
-       // let isCheckedInputJSApplications;
-       // let isCheckedInputJSWeb;
+        // let isCheckedInputJSFundamentals;
+        // let isCheckedInputJSAdvanced;
+        // let isCheckedInputJSApplications;
+        // let isCheckedInputJSWeb;
  
-       // $inputJSFundamentals.input(function() {
-       //     if ($(this).is(':checked')) {
-       //         isCheckedInputJSFundamentals = true;
-       //     }
-       //     });
+        // $inputJSFundamentals.input(function() {
+        //     if ($(this).is(':checked')) {
+        //         isCheckedInputJSFundamentals = true;
+        //     }
+        // });
  
-       //  $inputJSAdvanced.change(function() {
-       //      if ($(this).is(':checked')) {
-       //         isCheckedInputJSAdvanced = true;
-       //      }
-       //  });      
+        //  $inputJSAdvanced.change(function() {
+        //      if ($(this).is(':checked')) {
+        //         isCheckedInputJSAdvanced = true;
+        //      }
+        //  });      
  
-       let cost = 0;
-       if (isCheckedInputJSFundamentals) {
-          let $li = $('<li>');
-          $li.text('JS-Fundamentals');
-          $ulMyCourses.append($li);
-          cost += 170;
-       }
+        let cost = 0;
+        if (isCheckedInputJSFundamentals) {
+            let $li = $('<li>');
+            $li.text('JS-Fundamentals');
+            $ulMyCourses.append($li);
+            cost += 170;
+        }
  
-       if (isCheckedInputJSAdvanced) {
-          let $li = $('<li>');
-          $li.text('JS-Advanced');
-          $ulMyCourses.append($li);
-          cost += 180;
-       }
+        if (isCheckedInputJSAdvanced) {
+            let $li = $('<li>');
+            $li.text('JS-Advanced');
+            $ulMyCourses.append($li);
+            cost += 180;
+        }
  
-       if (isCheckedInputJSApplications) {
-          let $li = $('<li>');
-          $li.text('JS-Applications');
-          $ulMyCourses.append($li);
-          cost += 190;
-       }
+        if (isCheckedInputJSApplications) {
+            let $li = $('<li>');
+            $li.text('JS-Applications');
+            $ulMyCourses.append($li);
+            cost += 190;
+        }
  
-       if (isCheckedInputJSWeb) {
-          let $li = $('<li>');
-          $li.text('JS-Web');
-          $ulMyCourses.append($li);
-          cost += 490;
-       }
+        if (isCheckedInputJSWeb) {
+            let $li = $('<li>');
+            $li.text('JS-Web');
+            $ulMyCourses.append($li);
+            cost += 490;
+        }
  
-       if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced) {
-          cost -= 180 * 0.1;
-       }
+        if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced) {
+            cost -= 180 * 0.1;
+        }
  
-       if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced && isCheckedInputJSApplications) {
-          cost -= 31.32;
-       }
+        if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced && isCheckedInputJSApplications) {
+            cost -= 31.32;
+        }
        
-       if (isCheckedinputRadioOnline) {
-          cost -= cost * 0.06;
-       }
+        if (isCheckedinputRadioOnline) {
+            cost -= cost * 0.06;
+        }
  
-       if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced && isCheckedInputJSApplications && isCheckedInputJSWeb) {
-          let $li = $('<li>');
-          $li.text('HTML and CSS');
-          $ulMyCourses.append($li);
-       }
+        if (isCheckedInputJSFundamentals && isCheckedInputJSAdvanced && isCheckedInputJSApplications && isCheckedInputJSWeb) {
+            let $li = $('<li>');
+            $li.text('HTML and CSS');
+            $ulMyCourses.append($li);
+        }
  
-       cost = Math.floor(cost);
+         cost = Math.floor(cost);
  
-       $pCost.text(`Cost: ${cost}.00 BGN`);
-    } 
+         $pCost.text(`Cost: ${cost}.00 BGN`);
+     } 
  }
 
- //in Judge must be paste without this below:
+ //It must be paste in Judge without this below:
  solve();
