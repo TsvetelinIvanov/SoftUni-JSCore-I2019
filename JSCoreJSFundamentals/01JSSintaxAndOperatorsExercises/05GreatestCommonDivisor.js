ca2194d025ffbe6806f322720be8c365f1edd4e9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-01-31 06:29:05 +0200

    Update 05GreatestCommonDivisor.js

--- JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/05GreatestCommonDivisor.js
+++ JSCoreJSFundamentals/01JSSintaxAndOperatorsExercises/05GreatestCommonDivisor.js
@@ -1,7 +1,7 @@
 function findGreatestCommonDivisor(firstNumber, secondNumber) {
     let lowerNumber = Math.min(firstNumber, secondNumber);
     for (let i = lowerNumber; i > 0; i--) {
-        if (firstNumber % i === 0 && secondNumber % i === 0){
+        if (firstNumber % i === 0 && secondNumber % i === 0) {
             console.log(i);
             break;
         }
