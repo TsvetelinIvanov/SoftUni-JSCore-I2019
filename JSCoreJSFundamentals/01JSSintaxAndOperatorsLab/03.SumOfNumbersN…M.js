5a68fc13e1296593b8835faa5e2f830dd1a51171
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-01 05:24:03 +0200

    Update 03.SumOfNumbersN…M.js

--- JSCoreJSFundamentals/01JSSintaxAndOperatorsLab/03.SumOfNumbersN…M.js
+++ JSCoreJSFundamentals/01JSSintaxAndOperatorsLab/03.SumOfNumbersN…M.js
@@ -2,7 +2,7 @@ function solve(n, m) {
     let number1 = Number(n);
     let number2 = Number(m);
     let result = 0;
-    for (let i = number1; i <= number2; i++){
+    for (let i = number1; i <= number2; i++) {
         result += i;
     }
     
