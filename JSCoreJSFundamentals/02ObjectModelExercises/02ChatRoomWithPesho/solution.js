b185270dfca210f0180020569573e1009f556099
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-02 19:20:57 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/02ChatRoomWithPesho/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/02ChatRoomWithPesho/solution.js
@@ -1,30 +1,31 @@
 function solve() {
- let buttons = Array.from(document.getElementsByTagName('button'));
- let inputs = Array.from(document.getElementsByTagName('input'));
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    let inputs = Array.from(document.getElementsByTagName('input'));
  
- buttons.forEach((button) => {
-    button.addEventListener('click', function (event) {
-        let div = document.createElement('div');
-        let span = document.createElement('span');
-        let p = document.createElement('p');
-        let senderButtonName = event.target.name;
-        if (senderButtonName === 'myBtn') {
-            span.textContent = 'Me';
-            p.textContent = document.getElementById('myChatBox').value;
-            div.style.textAlign = 'left';
-        } else if (senderButtonName === 'peshoBtn') {
-            span.textContent = 'Pesho';
-            p.textContent = document.getElementById('peshoChatBox').value;
-            div.style.textAlign = 'right';
-        }
+    buttons.forEach((button) => {
+        button.addEventListener('click', function(event) {
+            let div = document.createElement('div');
+            let span = document.createElement('span');
+            let p = document.createElement('p');
+            let senderButtonName = event.target.name;
+            if (senderButtonName === 'myBtn') {
+                span.textContent = 'Me';
+                p.textContent = document.getElementById('myChatBox').value;
+                div.style.textAlign = 'left';
+            }
+            else if (senderButtonName === 'peshoBtn') {
+                span.textContent = 'Pesho';
+                p.textContent = document.getElementById('peshoChatBox').value;
+                div.style.textAlign = 'right';
+            }
 
-        div.appendChild(span);
-        div.appendChild(p);
-        let chatChronology = document.getElementById('chatChronology');
-        chatChronology.appendChild(div);
+            div.appendChild(span);
+            div.appendChild(p);
+            let chatChronology = document.getElementById('chatChronology');
+            chatChronology.appendChild(div);
      
-        inputs[0].value = '';
-        inputs[1].value = '';
+            inputs[0].value = '';
+            inputs[1].value = '';
+        })
     })
- })
 }
