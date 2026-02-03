b4884deb45485721e4538f3dfd6e44537f741b15
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-03 10:34:26 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/04LockedProfile/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/04LockedProfile/solution.js
@@ -1,18 +1,19 @@
 function solve() {
-   let profiles = Array.from(document.getElementsByClassName('profile'))
-   for (let profile of profiles) {
-      let button = profile.getElementsByTagName('button')[0];
-      button.addEventListener('click', function () {
-         let radioUnlocked = profile.getElementsByTagName('input')[1];
-         let hidenFieldId = profile.getElementsByTagName('div')[1].id;
-         let hiddenFields = document.getElementById(`${hidenFieldId}`);
-         if (radioUnlocked.checked && button.textContent === 'Show more') {
-            hiddenFields.style.display = 'block';
-            button.textContent = 'Hide it';
-         } else if(radioUnlocked.checked && button.textContent === 'Hide it') {
-            hiddenFields.style.display = 'none';
-            button.textContent = 'Show more';
-         }
-      })
-   }
+    let profiles = Array.from(document.getElementsByClassName('profile'));
+    for (let profile of profiles) {
+        let button = profile.getElementsByTagName('button')[0];
+        button.addEventListener('click', function() {
+            let radioUnlocked = profile.getElementsByTagName('input')[1];
+            let hidenFieldId = profile.getElementsByTagName('div')[1].id;
+            let hiddenFields = document.getElementById(`${hidenFieldId}`);
+            if (radioUnlocked.checked && button.textContent === 'Show more') {
+                hiddenFields.style.display = 'block';
+                button.textContent = 'Hide it';
+            }
+            else if(radioUnlocked.checked && button.textContent === 'Hide it') {
+                hiddenFields.style.display = 'none';
+                button.textContent = 'Show more';
+            }
+        })
+    }
 }
