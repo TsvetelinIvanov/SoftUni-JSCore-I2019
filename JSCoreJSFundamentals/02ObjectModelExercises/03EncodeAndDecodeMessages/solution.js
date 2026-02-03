40f84eb1cc9b5d266b3079cdf8e6a39a22656d9a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-03 09:26:19 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/03EncodeAndDecodeMessages/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/03EncodeAndDecodeMessages/solution.js
@@ -4,25 +4,25 @@ function solve() {
     buttons[0].addEventListener('click', encode);
     buttons[1].addEventListener('click', decode);
     function encode() {
-	let icputMessage = textAreas[0].value;
-	let encodedMessage = '';
-	icputMessage.split('').forEach((char) => {
-	    let asciiValue = char.charCodeAt(0) + 1;
-	    encodedMessage += String.fromCharCode(asciiValue);
-	});
+		let icputMessage = textAreas[0].value;
+		let encodedMessage = '';
+		icputMessage.split('').forEach((char) => {
+	    	let asciiValue = char.charCodeAt(0) + 1;
+	    	encodedMessage += String.fromCharCode(asciiValue);
+		});
 	    
-	textAreas[0].value = '';
-	textAreas[1].value = encodedMessage;
+		textAreas[0].value = '';
+		textAreas[1].value = encodedMessage;
     }
 
     function decode() {
-	let encodedMessage = textAreas[1].value;
-	let decodedMessage = '';
-	encodedMessage.split('').forEach((char) => {
-	    let asciiValue = char.charCodeAt(0) - 1;
-	    decodedMessage += String.fromCharCode(asciiValue);
-	});
+		let encodedMessage = textAreas[1].value;
+		let decodedMessage = '';
+		encodedMessage.split('').forEach((char) => {
+	    	let asciiValue = char.charCodeAt(0) - 1;
+	    	decodedMessage += String.fromCharCode(asciiValue);
+		});
 	    
-	textAreas[1].value = decodedMessage;
+		textAreas[1].value = decodedMessage;
     }
 }
