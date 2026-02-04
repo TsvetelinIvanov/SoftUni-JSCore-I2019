4da7cc509eac1ef6dd99a615d30a0fca193c5131
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-04 13:57:25 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/06SoftUniQuizz/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/06SoftUniQuizz/solution.js
@@ -7,28 +7,30 @@ function solve() {
     let hiddenSectionsCount = 0;
 
     buttons.forEach(button => {
-	button.addEventListener('click', event => {
-	    let parent = button.parentNode;
-	    let inputElements = Array.from(parent.getElementsByTagName('input'));
-	    inputElements.forEach(input => {
-		if (input.checked === true) {
-		    hiddenSectionsCount++;
-		    let answer = input.value;
-		    if (rightAnswers.includes(answer)) {
-			rightAnswersCount++;
-		    }
+		button.addEventListener('click', event => {
+	    	let parent = button.parentNode;
+	    	let inputElements = Array.from(parent.getElementsByTagName('input'));
+	    	inputElements.forEach(input => {
+				if (input.checked === true) {
+		    		hiddenSectionsCount++;
+		    		let answer = input.value;
+		    		if (rightAnswers.includes(answer)) {
+						rightAnswersCount++;
+		    		}
 
-		    if (button.textContent === 'Get the results') {
-			if (rightAnswersCount === 3) {
-			    resultElement.textContent = `You are recognized as top SoftUni fan!`;
-			} else {
-			    resultElement.textContent = `You have ${rightAnswersCount} right answers`;
-			}
-		    } else {
-			exerciseElement.children[hiddenSectionsCount].style.display = 'block';
-		    }
-		}
-	    });
-	});
+		    		if (button.textContent === 'Get the results') {
+						if (rightAnswersCount === 3) {
+			    			resultElement.textContent = `You are recognized as top SoftUni fan!`;
+						}
+						else {
+			    			resultElement.textContent = `You have ${rightAnswersCount} right answers`;
+						}
+		    		}
+					else {
+						exerciseElement.children[hiddenSectionsCount].style.display = 'block';
+		    		}
+				}
+	    	});
+		});
     });
 }
