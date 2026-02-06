6df967fdb4ea25c5ebac86812f1ec46cd1ce5d91
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-06 21:50:34 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/08TotoNumbers/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/08TotoNumbers/solution.js
@@ -2,35 +2,35 @@ function solve() {
     let button = document.querySelector('button');
     button.addEventListener('click', addTotoNumbers);
 	
-    function addTotoNumbers () {
-	let inputField = document.querySelector('input');
-	let inputArray = Array.from(inputField.value.split(' '));
-	let numbers = inputArray.map(Number);
+    function addTotoNumbers() {
+		let inputField = document.querySelector('input');
+		let inputArray = Array.from(inputField.value.split(' '));
+		let numbers = inputArray.map(Number);
 		
-	if (numbers.length !== 6){
-	    return;
-	}
+		if (numbers.length !== 6) {
+	    	return;
+		}
 
-	for (let number of numbers) {
-	    if (number < 1 || number > 49){
-		return;
-	    }
-	}
+		for (let number of numbers) {
+	    	if (number < 1 || number > 49) {
+				return;
+	    	}
+		}
 
-	//numbers.sort((a,b) => a - b);
-	let allNumbersDiv = document.getElementById('allNumbers');
-	for (let i = 1; i <= 49; i++) {
-	    let numberDiv = document.createElement('div');
-	    numberDiv.textContent = i;
-	    numberDiv.classList.add('numbers');
-	    if (numbers.includes(i)) {
-		numberDiv.style.backgroundColor = 'orange';
-	    }
+		//numbers.sort((a,b) => a - b);
+		let allNumbersDiv = document.getElementById('allNumbers');
+		for (let i = 1; i <= 49; i++) {
+	    	let numberDiv = document.createElement('div');
+	    	numberDiv.textContent = i;
+	    	numberDiv.classList.add('numbers');
+	    	if (numbers.includes(i)) {
+				numberDiv.style.backgroundColor = 'orange';
+	    	}
 		
-	    allNumbersDiv.appendChild(numberDiv);
-	}
+	    	allNumbersDiv.appendChild(numberDiv);
+		}
 	    
-	button.setAttribute('disabled', true);
-	inputField.setAttribute('disabled', true);		
+		button.setAttribute('disabled', true);
+		inputField.setAttribute('disabled', true);		
     }
 }
