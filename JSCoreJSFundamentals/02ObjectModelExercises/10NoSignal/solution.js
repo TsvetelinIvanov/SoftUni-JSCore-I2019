2243d8e6eb40820a5483482d9967c90d55ca91b5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-07 09:10:50 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelExercises/10NoSignal/solution.js
+++ JSCoreJSFundamentals/02ObjectModelExercises/10NoSignal/solution.js
@@ -9,11 +9,11 @@ function solve() {
     // 	    noSignalDiv.style.top = `${horizontalRandomNumber}%`;
     // 	    noSignalDiv.style.left = `${verticalRandomNumber}vh`;
     // }, 2000);
-    setInterval(function () {
-	let horizontalRandomNumber = Math.floor(Math.random() * 81) + 1;
-	let verticalRandomNumber = Math.floor(Math.random() * 45) + 1;
-	noSignalDiv.style.position = `absolute`;
-	noSignalDiv.style.top = `${horizontalRandomNumber}%`;
-	noSignalDiv.style.left = `${verticalRandomNumber}vh`;
+    setInterval(function() {
+		let horizontalRandomNumber = Math.floor(Math.random() * 81) + 1;
+		let verticalRandomNumber = Math.floor(Math.random() * 45) + 1;
+		noSignalDiv.style.position = `absolute`;
+		noSignalDiv.style.top = `${horizontalRandomNumber}%`;
+		noSignalDiv.style.left = `${verticalRandomNumber}vh`;
     }, 2000);
 }
