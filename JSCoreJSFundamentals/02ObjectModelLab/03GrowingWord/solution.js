b757a86cc40936fff343be6df1c33d202e59a5a1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-09 06:39:19 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelLab/03GrowingWord/solution.js
+++ JSCoreJSFundamentals/02ObjectModelLab/03GrowingWord/solution.js
@@ -1,18 +1,18 @@
 function solve() {
     let clicksCount = 0;
     document.querySelector('button').addEventListener('click', () => {
-       let p = document.querySelector('#exercise p');
-       if(clicksCount % 3 === 0) {
-          p.style.color = "blue";
-       } 
-       else if (clicksCount % 3 === 1) {
-          p.style.color = "green";
-       } 
-       else if(clicksCount % 3 === 2) {
-          p.style.color = "red";
-       }
+        let p = document.querySelector('#exercise p');
+        if (clicksCount % 3 === 0) {
+            p.style.color = "blue";
+        } 
+        else if (clicksCount % 3 === 1) {
+            p.style.color = "green";
+        } 
+        else if (clicksCount % 3 === 2) {
+            p.style.color = "red";
+        }
       
-       clicksCount++;
-       p.style.fontSize = `${clicksCount * 2}px`; 
+        clicksCount++;
+        p.style.fontSize = `${clicksCount * 2}px`; 
     });
 }
