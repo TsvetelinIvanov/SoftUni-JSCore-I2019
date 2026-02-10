332eb996df0649272ed19e6d04ef5dd3f7916d26
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-10 18:40:41 +0200

    Update solution.js

--- JSCoreJSFundamentals/02ObjectModelLab/05VisitedSites/solution.js
+++ JSCoreJSFundamentals/02ObjectModelLab/05VisitedSites/solution.js
@@ -5,7 +5,7 @@ function solve() {
             let spanElement = a.parentNode.children[1];
             let spanElementArray = spanElement.textContent.split(' ');
             let visitedCounter = Number(spanElementArray[1]);
-            visitedCounter++
+            visitedCounter++;
             spanElementArray[1] = visitedCounter;
             spanElement.textContent = spanElementArray.join(' ');
         });
