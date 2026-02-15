a085cd2c43ef31b7f22b7538898945e271cd65bf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-15 12:11:30 +0200

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/10Factors/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowExercises/10Factors/solution.js
@@ -1,16 +1,16 @@
 function solve() {
-   let number = Number(document.getElementById('num').value);
-   let resultElement = document.getElementById('result');
-   resultElement.textContent = getFactors(number);
+    let number = Number(document.getElementById('num').value);
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = getFactors(number);
 
-   function getFactors(number) {
-      let factors = [];
-      for (let i = 1; i <= number; i++) {
-         if (number % i === 0) {
-            factors.push(i);
-         }                  
-      }
+    function getFactors(number) {
+        let factors = [];
+        for (let i = 1; i <= number; i++) {
+            if (number % i === 0) {
+                factors.push(i);
+            }                  
+        }
 
-      return factors.join(' ');
-   }
+        return factors.join(' ');
+    }
 }
