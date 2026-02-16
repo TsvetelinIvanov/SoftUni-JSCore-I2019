9f551f460deac3bcf3a621e7bcf0caf7b3bca9c4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-02-16 16:32:05 +0200

    Update solution.js

--- JSCoreJSFundamentals/03FunctionsAndLogicFlowLab/01MultiplicationTable/solution.js
+++ JSCoreJSFundamentals/03FunctionsAndLogicFlowLab/01MultiplicationTable/solution.js
@@ -11,17 +11,17 @@ function solve() {
 
     function findWrongInput(multiplicationStartNumber, multiplier) {
         if (multiplicationStartNumber > multiplier) {
-        //document.getElementById('result').innerHTML = 'Try with other numbers.';
-        resultElement.innerHTML = 'Try with other numbers.';
+            //document.getElementById('result').innerHTML = 'Try with other numbers.';
+            resultElement.innerHTML = 'Try with other numbers.';
+        }
     }
-  }
 
-  function showResults(multiplicationStartNumber, multiplier) {
-      for (let i = multiplicationStartNumber; i <= multiplier; i++) {
-      let result = i * multiplier;
-      let p = document.createElement('p');
-      p.innerHTML = `${i} * ${multiplier} = ${result}`;
-      resultElement.appendChild(p);
+    function showResults(multiplicationStartNumber, multiplier) {
+        for (let i = multiplicationStartNumber; i <= multiplier; i++) {
+            let result = i * multiplier;
+            let p = document.createElement('p');
+            p.innerHTML = `${i} * ${multiplier} = ${result}`;
+            resultElement.appendChild(p);
+        }
     }
-  }
 }
