287e65e45824b0521e11721d4ffddbb0ec273916
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2026-03-01 00:20:01 +0200

    Update 08CityMarkets.js

--- JSCoreJSFundamentals/06ObjectsAndJSONLab/08CityMarkets.js
+++ JSCoreJSFundamentals/06ObjectsAndJSONLab/08CityMarkets.js
@@ -1,7 +1,7 @@
 function calculateProductsIncomeForTown(dataArray) {
     let towns = {};
     
-    for(let townData of dataArray) {        
+    for (let townData of dataArray) {        
         let [townName, productName, productIncomeData] = townData.split(' -> ');        
         let productIncome = productIncomeData.split(' : ').map(Number).reduce((a, b) => a * b);
 
@@ -17,16 +17,16 @@ function calculateProductsIncomeForTown(dataArray) {
     }
 
     let outputString = '';
-    for(let town in towns) {
+    for (let town in towns) {
         outputString += `Town - ${town}\n`;
-        for(let product in towns[town]) {
+        for (let product in towns[town]) {
             outputString += `$$$${product} : ${towns[town][product]}\n`;
         }
     }
     
     outputString = outputString.substring(0, outputString.length - 1);
 
-    console.log(outputString)
+    console.log(outputString);
 }
 
 calculateProductsIncomeForTown(['Sofia -> Laptops HP -> 200 : 2000', 'Sofia -> Raspberry -> 200000 : 1500', 'Sofia -> Audi Q7 -> 200 : 100000', 'Montana -> Portokals -> 200000 : 1', 'Montana -> Qgodas -> 20000 : 0.2', 'Montana -> Chereshas -> 1000 : 0.3'])
